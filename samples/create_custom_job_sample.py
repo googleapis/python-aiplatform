b77e321a431b57d65b68d39dc123070f977a17ba
Vinny Senthil <vinnysenthil@gmail.com>
2020-09-30 18:29:34 -0700

    Add two extra samples + tests

---
+++ samples/create_custom_job_sample.py
@@ -0,0 +1,60 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START aiplatform_create_custom_job_sample]
+from google.cloud import aiplatform
+
+
+def create_custom_job_sample(display_name: str, container_image_uri: str, project: str):
+    client_options = {"api_endpoint": "us-central1-aiplatform.googleapis.com"}
+    # Initialize client that will be used to create and send requests.
+    # This client only needs to be created once, and can be reused for multiple requests.
+    client = aiplatform.JobServiceClient(client_options=client_options)
+    location = "us-central1"
+    parent = "projects/{project}/locations/{location}".format(
+        project=project, location=location
+    )
+    custom_job = {
+        "display_name": display_name,
+        "job_spec": {
+            "worker_pool_specs": [
+                {
+                    "machine_spec": {
+                        "machine_type": "n1-standard-4",
+                        "accelerator_type": aiplatform.AcceleratorType.NVIDIA_TESLA_K80,
+                        "accelerator_count": 1,
+                    },
+                    "replica_count": 1,
+                    "container_spec": {
+                        "image_uri": container_image_uri,
+                        "command": [],
+                        "args": [],
+                    },
+                }
+            ]
+        },
+    }
+    response = client.create_custom_job(parent=parent, custom_job=custom_job)
+    print("response")
+    print(" name:", response.name)
+    print(" display_name:", response.display_name)
+    print(" state:", response.state)
+    print(" start_time:", response.start_time)
+    print(" end_time:", response.end_time)
+    print(" labels:", response.labels)
+    job_spec = response.job_spec
+    error = response.error
+
+
+# [END aiplatform_create_custom_job_sample]
