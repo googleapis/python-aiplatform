50526eccf73aa9944f70fa8733a1f4c3e7f76490
Vinny Senthil <vinnysenthil@gmail.com>
2020-10-07 14:21:17 -0700

    Add initial helper util functions and tests (#25)
    * Add initial helper util functions and tests
    
    * Make requested changes, add TODOs to utils.py
    
    * Fix indent and lint

---
+++ google/cloud/aiplatform/utils.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import re
+
+from typing import Dict
+from typing import Optional
+from collections import namedtuple
+
+from google.cloud.aiplatform.initializer import singleton as state
+
+SUPPORTED_REGIONS = ("us-central1", "europe-west4", "asia-east1")
+PROD_API_ENDPOINT = "aiplatform.googleapis.com"
+
+# TODO(b/170334193): Add support for resource names with non-integer IDs
+# TODO(b/170334098): Add support for resource names more than one level deep
+RESOURCE_NAME_PATTERN = re.compile(
+    r"^projects\/(?P<project>[\w-]+)\/locations\/(?P<location>[\w-]+)\/(?P<resource>\w+)\/(?P<id>\d+)$"
+)
+
+Fields = namedtuple(
+    "Fields",
+    [
+        "project",
+        "location",
+        "resource",
+        "id",
+    ],
+)
+
+
+def _match_to_fields(match: re.Match) -> Optional[Fields]:
+    """Normalize RegEx groups from resource name pattern Match to class Fields"""
+    if not match:
+        return None
+
+    return Fields(
+        project=match["project"],
+        location=match["location"],
+        resource=match["resource"],
+        id=match["id"],
+    )
+
+
+def validate_name(
+    resource_name: str, resource_noun: Optional[str] = None
+) -> Optional[Fields]:
+    """Validates and returns extracted fields from a fully-qualified resource name.
+    Returns None if name is invalid.
+
+    Args:
+        resource_name (str):
+            Required. A fully-qualified AI Platform (Unified) resource name
+
+        resource_noun (str):
+            A plural resource noun to validate the resource name against.
+            For example, you would pass "datasets" to validate
+            "projects/123/locations/us-central1/datasets/456".
+
+    Returns:
+        fields (Fields):
+            A named tuple containing four extracted fields from a resource name:
+            project, location, resource, and id. These fields can be used for
+            subsequent method calls in the SDK.
+    """
+    fields = _match_to_fields(RESOURCE_NAME_PATTERN.match(resource_name))
+
+    if not fields:
+        return None
+    if resource_noun and fields.resource != resource_noun:
+        return None
+
+    return fields
+
+
+def get_client_options(
+    location_override: Optional[str] = None, prediction_client: Optional[bool] = False
+) -> Dict[str, str]:
+    """Creates client_options for GAPIC service client using location and client type.
+
+    Args:
+        location_override (str):
+            Set this parameter to get client options for a location different
+            from location set by initializer. Must be a GCP region
+            supported by AI Platform (Unified).
+
+        prediction_client (bool):
+            True if service client is a PredictionServiceClient, otherwise
+            defaults to False. This is used to provide a prediction-specific
+            API endpoint.
+
+    Returns:
+        clients_options (dict):
+            A dictionary containing client_options with one key, for example
+            { "api_endpoint": "us-central1-aiplatform.googleapis.com" } or
+            { "api_endpoint": "asia-east1-prediction-aiplatform.googleapis.com" }
+    """
+    if not (state.location or location_override):
+        raise ValueError(
+            "No location found. Provide or initialize SDK with a location."
+        )
+
+    region = state.location if not location_override else location_override
+    region = region.lower()
+    prediction = "prediction-" if prediction_client else ""
+
+    if region not in SUPPORTED_REGIONS:
+        raise ValueError(
+            f"Unsupported region for AI Platform, select from {SUPPORTED_REGIONS}"
+        )
+
+    client_options = {"api_endpoint": f"{region}-{prediction}{PROD_API_ENDPOINT}"}
+
+    return client_options
