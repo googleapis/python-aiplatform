eff1a6ec7ed5f9824071d72c32de572ce3ae1846
sirtorry <torryyang@google.com>
2020-10-16 20:29:27 -0700

    address comments

--- google/cloud/aiplatform/lro.py
+++ google/cloud/aiplatform/lro.py
@@ -15,32 +15,34 @@
 # limitations under the License.
 #
 
-import threading
+import functools
 
 from google.api_core import operation as ga_operation
+from google.cloud.aiplatform_v1beta1.services.endpoint_service.transports.base import EndpointServiceTransport
+from google.protobuf import empty_pb2 as empty
 
+class LRO(ga_operation):
+    """A handler for Operation futures.
 
-class LRO:
-    """A handler for Operation futures"""
+    Args:
+        operation (google.longrunning.operations_pb2.Operation): The
+            initial operation.
+        result_type (func:`type`): The protobuf type for the operation's
+            result.
+    """
 
-    def __init__(self, operation, operations_client, result_type, **kwargs):
-        self._operation_future = ga_operation.from_gapic(
-            operation, operations_client, result_type, **kwargs
-        )
-        self.result = None
-        self.exception = None
-        self._poll_until_finish()
+    def __init__(self, operation, result_type=empty.Empty, **kwargs):
+        operations_client = EndpointServiceTransport().operations_client
+        refresh = functools.partial(operations_client.get_operation, operation.name)
+        cancel = functools.partial(operations_client.cancel_operation, operation.name)
+        super().__init__(operation, refresh, cancel, result_type, **kwargs)
+        self.operation_name = operation.name
+        self.add_done_callback(callback)
 
     def callback(operation_future):
-        self._operation_future._set_result_from_operation()
-        self.result = operation_future.result()
-        self.exception = operation_future.exception()
+        """Callback used to set results of operation future.
 
-    def done(self):
-        return self._operation_future.done()
-
-    def _poll_until_finish(self):
-        self._operation_future.add_done_callback(callback)
-
-    def running(self):
-        return self._operation_future.running()
+        Args:
+            operation_future (ga_operation.Operation): operation to set result of
+        """
+        operation_future._set_result_from_operation()
