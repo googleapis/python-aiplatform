bb285077806720601b3736e7e77ba2f3da9eea46
sirtorry <torryyang@google.com>
2020-10-21 00:04:37 -0700

    fix unit test

--- tests/unit/aiplatform/test_lro.py
+++ tests/unit/aiplatform/test_lro.py
@@ -17,11 +17,19 @@ import mock
 from google.api_core import operation
 from google.cloud.aiplatform import base
 from google.cloud.aiplatform import lro
+from google.cloud.aiplatform_v1beta1.services.model_service.client import (
+    ModelServiceClient
+)
 from google.longrunning import operations_pb2
 from google.protobuf import empty_pb2 as empty
 from google.protobuf import struct_pb2
 
 TEST_OPERATION_NAME = "test/operation"
+
+
+class AiPlatformResourceNounImpl(base.AiPlatformResourceNoun):
+    client_class = ModelServiceClient
+    _is_client_prediction_client = False
 
 
 def make_operation_proto(
@@ -61,43 +69,43 @@ def make_operation_future(client_operations_responses=None):
 
 def test_constructor():
     operation_future = make_operation_future()
-    lro = lro.LRO(operation_future)
+    test_lro = lro.LRO(operation_future)
 
-    assert lro._operation_future.name == TEST_OPERATION_NAME
-    assert lro._operation_future._result_type is empty.Empty
-    assert lro._operation_future.metadata() is None
-    assert lro._operation_future.done() is False
-    assert lro._operation_future.running()
-    assert len(lro._operation_future._done_callbacks) is 0
+    assert test_lro._operation_future.name == TEST_OPERATION_NAME
+    assert test_lro._operation_future._result_type is empty.Empty
+    assert test_lro._operation_future.metadata() is None
+    assert test_lro._operation_future.done() is False
+    assert test_lro._operation_future.running()
+    assert len(test_lro._operation_future._done_callbacks) is 0
 
 
 def test_constructor_with_update():
     operation_future = make_operation_future()
-    resource_noun_obj = base.AiPlatformResourceNoun()
+    resource_noun_obj = AiPlatformResourceNounImpl()
     result_key = "name"
     api_get = mock.Mock(spec=["__call__"])
-    lro = lro.LRO(operation_future, resource_noun_obj, result_key, api_get)
+    test_lro = lro.LRO(operation_future, resource_noun_obj, result_key, api_get)
 
-    assert len(lro._operation_future._done_callbacks) is 1
+    assert len(test_lro._operation_future._done_callbacks) is 1
 
 
 def test_add_update_resource_callback():
     operation_future = make_operation_future()
-    lro = lro.LRO(operation_future)
-    resource_noun_obj = base.AiPlatformResourceNoun()
+    test_lro = lro.LRO(operation_future)
+    resource_noun_obj = AiPlatformResourceNounImpl()
     result_key = "name"
     api_get = mock.Mock(spec=["__call__"])
-    lro.add_update_resource_callback(resource_noun_obj, result_key, api_get)
+    test_lro.add_update_resource_callback(resource_noun_obj, result_key, api_get)
 
-    assert len(lro._operation_future._done_callbacks) is 1
+    assert len(test_lro._operation_future._done_callbacks) is 1
 
 
 def test_operation_future():
     operation_future = make_operation_future()
-    lro = lro.LRO(operation_future)
+    test_lro = lro.LRO(operation_future)
 
-    assert lro.operation_future.name == TEST_OPERATION_NAME
-    assert lro.operation_future._result_type is empty.Empty
-    assert lro.operation_future.metadata() is None
-    assert lro.operation_future.done() is False
-    assert lro.operation_future.running()
+    assert test_lro.operation_future.name == TEST_OPERATION_NAME
+    assert test_lro.operation_future._result_type is empty.Empty
+    assert test_lro.operation_future.metadata() is None
+    assert test_lro.operation_future.done() is False
+    assert test_lro.operation_future.running()
