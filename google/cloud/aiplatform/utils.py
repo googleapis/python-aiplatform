52078ade077ea314920e8a39b10bd8f023ce88c3
Sasha Sobran <asobran@google.com>
2020-10-26 15:05:16 -0400

    style: Black formatting to specified version

--- google/cloud/aiplatform/utils.py
+++ google/cloud/aiplatform/utils.py
@@ -45,10 +45,7 @@ RESOURCE_NAME_PATTERN = re.compile(
 )
 RESOURCE_ID_PATTERN = re.compile(r"^\d+$")
 
-Fields = namedtuple(
-    "Fields",
-    ["project", "location", "resource", "id"],
-)
+Fields = namedtuple("Fields", ["project", "location", "resource", "id"],)
 
 
 def _match_to_fields(match: Match) -> Optional[Fields]:
