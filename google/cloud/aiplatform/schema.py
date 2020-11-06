5b098a400e5fe3bd387fdd7ce7f158d067be5b7d
Ivan Cheung <ivans.mailbox@gmail.com>
2020-11-05 20:41:33 -0500

    feat: Added tabular data support from BQ and GCS during dataset creation (#40)
    * feat: Add tabular import support from GCS and BQ
    
    * fix: Ran linter again with following commands
    
    pip3 install flake8 black==19.10b0
    black docs google tests noxfile.py setup.py
    flake8 google tests
    
    * fix: Moved instantiation after validation and renamed a _import_gcs function
    
    Co-authored-by: Ivan Cheung <ivanmkc@google.com>

---
+++ google/cloud/aiplatform/schema.py
@@ -0,0 +1,11 @@
+class training_job:
+    class definition:
+        custom_task = "gs://google-cloud-aiplatform/schema/trainingjob/definition/custom_task_1.0.0.yaml"
+
+
+class dataset:
+    class metadata:
+        tabular = (
+            "gs://google-cloud-aiplatform/schema/dataset/metadata/tabular_1.0.0.yaml"
+        )
+        image = "gs://google-cloud-aiplatform/schema/dataset/metadata/image_1.0.0.yaml"
