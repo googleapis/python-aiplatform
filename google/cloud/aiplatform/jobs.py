c2e148cb0bb33903ee97cc4983d4493ce9485c79
Vinny Senthil <vinnysenthil@gmail.com>
2020-11-16 22:48:45 -0800

    Init Job and BatchPredictionJob class, unit tests

--- google/cloud/aiplatform/jobs.py
+++ google/cloud/aiplatform/jobs.py
@@ -15,10 +15,214 @@
 # limitations under the License.
 #
 
+from abc import abstractclassmethod
+from typing import Iterable, Optional, Union
 
-class Job:
+from google.cloud import storage
+from google.cloud import bigquery
+
+from google.auth import credentials as auth_credentials
+
+from google.cloud.aiplatform import base
+from google.cloud.aiplatform.gapic import JobState
+from google.cloud.aiplatform.gapic import JobServiceClient
+from google.cloud.aiplatform.utils import full_resource_name
+
+
+class _Job(base.AiPlatformResourceNoun):
+    """
+    Class that represents a general Job resource in AI Platform (Unified).
+    Cannot be directly instantiated.
+
+    Serves as base class to specific Job types, i.e. BatchPredictionJob or
+    DataLabelingJob to re-use shared functionality.
+
+    Subclasses requires one class attribute:
+
+    getter_method (str): The name of JobServiceClient getter method for specific
+    Job type, i.e. 'get_custom_job' for CustomJob
+    """
+
+    client_class = JobServiceClient
+    _is_client_prediction_client = False
+
+    @property
+    @abstractclassmethod
+    def getter_method(cls) -> str:
+        """Name of getter method of Job subclass, i.e. 'get_custom_job' for CustomJob"""
+        pass
+
+    def __init__(
+        self,
+        valid_job_name: str,
+        project: Optional[str] = None,
+        location: Optional[str] = None,
+        credentials: Optional[auth_credentials.Credentials] = None,
+    ):
+        """
+        Retrives Job subclass resource by calling a subclass-specific getter method.
+
+        Args:
+             valid_job_name (str):
+                A validated, fully-qualified Job resource name. For example:
+                'projects/.../locations/.../batchPredictionJobs/456' or
+                'projects/.../locations/.../customJobs/789'
+            project: Optional[str] = None,
+                Optional project to retrieve Job subclass from. If not set,
+                project set in aiplatform.init will be used.
+            location: Optional[str] = None,
+                Optional location to retrieve Job subclass from. If not set,
+                location set in aiplatform.init will be used.
+            credentials: Optional[auth_credentials.Credentials] = None,
+                Custom credentials to use. If not set, credentials set in
+                aiplatform.init will be used.
+        """
+        super().__init__(project=project, location=location, credentials=credentials)
+        self.job_subclass_getter_method = getattr(self.api_client, self.getter_method)
+        self._gca_resource = self.job_subclass_getter_method(name=valid_job_name)
+
+    def status(self) -> JobState:
+        """Fetch Job again and return the current JobState.
+
+        Returns:
+            state (JobState):
+                Enum that describes the state of a AI Platform job.
+        """
+
+        # Fetch the Job again for most up-to-date job state
+        self._gca_resource = self.job_subclass_getter_method(
+            name=self._gca_resource.name
+        )
+
+        return self._gca_resource.state
+
+
+class BatchPredictionJob(_Job):
+
+    getter_method = "get_batch_prediction_job"
+
+    def __init__(
+        self,
+        batch_prediction_job_name: str,
+        project: Optional[str] = None,
+        location: Optional[str] = None,
+        credentials: Optional[auth_credentials.Credentials] = None,
+    ):
+        """
+        Retrieves a BatchPredictionJob resource and instantiates its representation.
+
+        Args:
+            batch_prediction_job_name (str):
+                Required. A fully-qualified BatchPredictionJob resource name or ID.
+                Example: "projects/.../locations/.../batchPredictionJobs/456" or
+                "456" when project and location are initialized or passed.
+            project: Optional[str] = None,
+                Optional project to retrieve BatchPredictionJob from. If not set,
+                project set in aiplatform.init will be used.
+            location: Optional[str] = None,
+                Optional location to retrieve BatchPredictionJob from. If not set,
+                location set in aiplatform.init will be used.
+            credentials: Optional[auth_credentials.Credentials] = None,
+                Custom credentials to use. If not set, credentials set in
+                aiplatform.init will be used.
+        """
+        valid_batch_prediction_job_name = full_resource_name(
+            resource_name=batch_prediction_job_name,
+            resource_noun="batchPredictionJobs",
+            project=project,
+            location=location,
+        )
+
+        super().__init__(
+            valid_job_name=valid_batch_prediction_job_name,
+            project=project,
+            location=location,
+            credentials=credentials,
+        )
+
+    def iter_outputs(
+        self, bq_query_limit: Optional[int] = 100
+    ) -> Iterable[Union[storage.Blob, bigquery.job.QueryJob]]:
+        """Returns an Iterable object to traverse the output files, either a list
+        of GCS Blobs or a BigQuery QueryJob depending on the output config set
+        when the BatchPredictionJob was created.
+
+        Args:
+            bq_query_limit: Optional[int] = 100
+                Limit on rows to select from prediction table in BigQuery dataset.
+                Only used when retrieving predictions from a bigquery_destination_prefix.
+                Default is 100.
+
+        Returns:
+            Iterable[Union[storage.Blob, bigquery.job.QueryJob]]:
+                Either a list of GCS Blob objects within the prediction output
+                directory or an iterable QueryJob with predictions.
+
+        Raises:
+            RuntimeError:
+                If BatchPredictionJob is in a JobState other than SUCCEEDED,
+                since outputs cannot be retrieved until the Job has finished.
+            NotImplementedError:
+                If BatchPredictionJob succeeded and output_info does not have a
+                GCS or BQ output provided.
+        """
+
+        job_status = self.status()
+
+        if job_status != JobState.JOB_STATE_SUCCEEDED:
+            raise RuntimeError(
+                f"Cannot read outputs until BatchPredictionJob has succeeded, "
+                f"current status: {job_status}"
+            )
+
+        output_info = self._gca_resource.output_info
+
+        # GCS Destination, return Blobs
+        if output_info.gcs_output_directory not in ("", None):
+            storage_client = storage.Client()
+            blobs = storage_client.list_blobs(output_info.gcs_output_directory)
+            return blobs
+
+        # BigQuery Destination, return QueryJob
+        elif output_info.bigquery_output_dataset not in ("", None):
+            bq_client = bigquery.Client()
+            bigquery.Client
+
+            # Format from service is `bq://projectId.bqDatasetId`
+            bq_dataset = output_info.bigquery_output_dataset
+
+            if bq_dataset.startswith("bq://"):
+                bq_dataset = bq_dataset[5:]
+            if bq_dataset.endswith(("/", ".")):
+                bq_dataset = bq_dataset[:-1]
+
+            # # Split project ID and BQ dataset ID
+            _, bq_dataset_id = bq_dataset.split(".", 1)
+
+            query_limit = f"LIMIT {bq_query_limit}" if bq_query_limit else ""
+            query = f"SELECT * FROM {bq_dataset_id}.predictions {query_limit}"
+            query_job = bq_client.query(query)
+
+            return query_job
+
+        # Unknown Destination type
+        else:
+            raise NotImplementedError(
+                f"Unsupported batch prediction output location, here are details"
+                f"on your prediction output:\n{output_info}"
+            )
+
+
+class CustomJob(_Job):
+    getter_method = "get_custom_job"
     pass
 
 
-class BatchPredictionJob(Job):
+class DataLabelingJob(_Job):
+    getter_method = "get_data_labeling_job"
+    pass
+
+
+class HyperparameterTuningJob(_Job):
+    getter_method = "get_hyperparameter_tuning_job"
     pass
---
+++ tests/unit/aiplatform/test_jobs.py
@@ -0,0 +1,268 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import pytest
+
+from importlib import reload
+from unittest.mock import patch
+
+from google.cloud.storage import Blob
+from google.cloud.storage import Client as StorageClient
+from google.cloud.bigquery import Client as BqClient
+from google.cloud.bigquery import QueryJob
+
+from google.cloud import aiplatform
+from google.cloud.aiplatform import initializer
+from google.cloud.aiplatform import BatchPredictionJob
+from google.cloud.aiplatform.jobs import _Job
+
+from google.cloud.aiplatform.gapic import JobServiceClient
+from google.cloud.aiplatform.gapic import BatchPredictionJob as GapicBatchPredictionJob
+from google.cloud.aiplatform.gapic import JobState
+from google.cloud.aiplatform.gapic import GcsSource
+from google.cloud.aiplatform.gapic import GcsDestination
+from google.cloud.aiplatform.gapic import BigQuerySource
+from google.cloud.aiplatform.gapic import BigQueryDestination
+
+
+_TEST_PROJECT = "test-project"
+_TEST_LOCATION = "us-central1"
+_TEST_ID = "1028944691210842416"
+_TEST_ALT_ID = "8834795523125638878"
+_TEST_DISPLAY_NAME = "my_job_1234"
+_TEST_BQ_DATASET_ID = "bqDatasetId"
+_TEST_BQ_JOB_ID = "123459876"
+_TEST_GCS_BUCKET_NAME = "my-bucket"
+
+_TEST_BQ_PATH = f"bq://projectId.{_TEST_BQ_DATASET_ID}"
+_TEST_GCS_BUCKET_PATH = f"gs://{_TEST_GCS_BUCKET_NAME}"
+_TEST_GCS_JSONL_SOURCE_URI = f"{_TEST_GCS_BUCKET_PATH}/bp_input_config.jsonl"
+_TEST_PARENT = f"projects/{_TEST_PROJECT}/locations/{_TEST_LOCATION}"
+
+_TEST_MODEL_NAME = (
+    f"projects/{_TEST_PROJECT}/locations/{_TEST_LOCATION}/models/{_TEST_ALT_ID}"
+)
+_TEST_BATCH_PREDICTION_NAME = f"projects/{_TEST_PROJECT}/locations/{_TEST_LOCATION}/batchPredictionJobs/{_TEST_ID}"
+
+_TEST_JOB_STATE_SUCCESS = JobState(4)
+_TEST_JOB_STATE_RUNNING = JobState(3)
+_TEST_JOB_STATE_PENDING = JobState(2)
+
+_TEST_GCS_INPUT_CONFIG = GapicBatchPredictionJob.InputConfig(
+    instances_format="jsonl", gcs_source=GcsSource(uris=[_TEST_GCS_JSONL_SOURCE_URI]),
+)
+_TEST_GCS_OUTPUT_CONFIG = GapicBatchPredictionJob.OutputConfig(
+    predictions_format="jsonl",
+    gcs_destination=GcsDestination(output_uri_prefix=_TEST_GCS_BUCKET_PATH),
+)
+
+_TEST_BQ_INPUT_CONFIG = GapicBatchPredictionJob.InputConfig(
+    instances_format="bigquery",
+    bigquery_source=BigQuerySource(input_uri=_TEST_BQ_PATH),
+)
+_TEST_BQ_OUTPUT_CONFIG = GapicBatchPredictionJob.OutputConfig(
+    predictions_format="bigquery",
+    bigquery_destination=BigQueryDestination(output_uri=_TEST_BQ_PATH),
+)
+
+_TEST_GCS_OUTPUT_INFO = GapicBatchPredictionJob.OutputInfo(
+    gcs_output_directory=_TEST_GCS_BUCKET_PATH
+)
+_TEST_BQ_OUTPUT_INFO = GapicBatchPredictionJob.OutputInfo(
+    bigquery_output_dataset=_TEST_BQ_PATH
+)
+
+_TEST_EMPTY_OUTPUT_INFO = GapicBatchPredictionJob.OutputInfo()
+
+_TEST_ITER_DIRS_BQ_QUERY = f"SELECT * FROM {_TEST_BQ_DATASET_ID}.predictions LIMIT 100"
+
+_TEST_GCS_BLOBS = [
+    Blob(name="some/path/prediction.jsonl", bucket=_TEST_GCS_BUCKET_NAME)
+]
+
+
+# TODO(b/171333554): Move reusable test fixtures to conftest.py file
+class TestJob:
+    def setup_method(self):
+        reload(initializer)
+        reload(aiplatform)
+
+    # Test Fixtures
+
+    @pytest.fixture
+    def get_batch_prediction_job_gcs_output_mock(self):
+        with patch.object(
+            JobServiceClient, "get_batch_prediction_job"
+        ) as get_batch_prediction_job_mock:
+            get_batch_prediction_job_mock.return_value = GapicBatchPredictionJob(
+                name=_TEST_BATCH_PREDICTION_NAME,
+                display_name=_TEST_DISPLAY_NAME,
+                model=_TEST_MODEL_NAME,
+                input_config=_TEST_GCS_INPUT_CONFIG,
+                output_config=_TEST_GCS_OUTPUT_CONFIG,
+                output_info=_TEST_GCS_OUTPUT_INFO,
+                state=_TEST_JOB_STATE_SUCCESS,
+            )
+            yield get_batch_prediction_job_mock
+
+    @pytest.fixture
+    def get_batch_prediction_job_bq_output_mock(self):
+        with patch.object(
+            JobServiceClient, "get_batch_prediction_job"
+        ) as get_batch_prediction_job_mock:
+            get_batch_prediction_job_mock.return_value = GapicBatchPredictionJob(
+                name=_TEST_BATCH_PREDICTION_NAME,
+                display_name=_TEST_DISPLAY_NAME,
+                model=_TEST_MODEL_NAME,
+                input_config=_TEST_GCS_INPUT_CONFIG,
+                output_config=_TEST_BQ_OUTPUT_CONFIG,
+                output_info=_TEST_BQ_OUTPUT_INFO,
+                state=_TEST_JOB_STATE_SUCCESS,
+            )
+            yield get_batch_prediction_job_mock
+
+    @pytest.fixture
+    def get_batch_prediction_job_empty_output_mock(self):
+        with patch.object(
+            JobServiceClient, "get_batch_prediction_job"
+        ) as get_batch_prediction_job_mock:
+            get_batch_prediction_job_mock.return_value = GapicBatchPredictionJob(
+                name=_TEST_BATCH_PREDICTION_NAME,
+                display_name=_TEST_DISPLAY_NAME,
+                model=_TEST_MODEL_NAME,
+                input_config=_TEST_GCS_INPUT_CONFIG,
+                output_config=_TEST_BQ_OUTPUT_CONFIG,
+                output_info=_TEST_EMPTY_OUTPUT_INFO,
+                state=_TEST_JOB_STATE_SUCCESS,
+            )
+            yield get_batch_prediction_job_mock
+
+    @pytest.fixture
+    def get_batch_prediction_job_running_bq_output_mock(self):
+        with patch.object(
+            JobServiceClient, "get_batch_prediction_job"
+        ) as get_batch_prediction_job_mock:
+            get_batch_prediction_job_mock.return_value = GapicBatchPredictionJob(
+                name=_TEST_BATCH_PREDICTION_NAME,
+                display_name=_TEST_DISPLAY_NAME,
+                model=_TEST_MODEL_NAME,
+                input_config=_TEST_GCS_INPUT_CONFIG,
+                output_config=_TEST_BQ_OUTPUT_CONFIG,
+                output_info=_TEST_BQ_OUTPUT_INFO,
+                state=_TEST_JOB_STATE_RUNNING,
+            )
+            yield get_batch_prediction_job_mock
+
+    @pytest.fixture
+    def storage_list_blobs_mock(self):
+        with patch.object(StorageClient, "list_blobs") as list_blobs_mock:
+            list_blobs_mock.return_value = _TEST_GCS_BLOBS
+            yield list_blobs_mock
+
+    @pytest.fixture
+    def bq_query_mock(self):
+        with patch.object(BqClient, "query") as query_mock:
+
+            query_mock.return_value = QueryJob(
+                job_id=_TEST_BQ_JOB_ID,
+                query=_TEST_ITER_DIRS_BQ_QUERY,
+                client=query_mock,
+            )
+            yield query_mock
+
+    # Unit Tests
+
+    def test_init_job_class(self):
+        """
+        Raises TypeError since abstract property 'getter_method' is not set,
+        the _Job class should only be instantiated through a child class.
+        """
+        with pytest.raises(TypeError):
+            _Job(valid_job_name=_TEST_BATCH_PREDICTION_NAME)
+
+    def test_init_batch_prediction_job_class(
+        self, get_batch_prediction_job_bq_output_mock
+    ):
+        aiplatform.BatchPredictionJob(
+            batch_prediction_job_name=_TEST_BATCH_PREDICTION_NAME
+        )
+        get_batch_prediction_job_bq_output_mock.assert_called_once_with(
+            name=_TEST_BATCH_PREDICTION_NAME
+        )
+
+    def test_batch_prediction_job_status(self, get_batch_prediction_job_bq_output_mock):
+        bp = aiplatform.BatchPredictionJob(
+            batch_prediction_job_name=_TEST_BATCH_PREDICTION_NAME
+        )
+
+        # get_batch_prediction() is called again here
+        bp_job_state = bp.status()
+
+        assert get_batch_prediction_job_bq_output_mock.call_count == 2
+        assert bp_job_state == _TEST_JOB_STATE_SUCCESS
+
+        get_batch_prediction_job_bq_output_mock.assert_called_with(
+            name=_TEST_BATCH_PREDICTION_NAME
+        )
+
+    def test_batch_prediction_iter_dirs_gcs(
+        self, get_batch_prediction_job_gcs_output_mock, storage_list_blobs_mock
+    ):
+        bp = BatchPredictionJob(batch_prediction_job_name=_TEST_BATCH_PREDICTION_NAME)
+        blobs = bp.iter_outputs()
+
+        storage_list_blobs_mock.assert_called_once_with(
+            _TEST_GCS_OUTPUT_INFO.gcs_output_directory
+        )
+
+        assert blobs == _TEST_GCS_BLOBS
+
+    def test_batch_prediction_iter_dirs_bq(
+        self, get_batch_prediction_job_bq_output_mock, bq_query_mock
+    ):
+        bp = BatchPredictionJob(batch_prediction_job_name=_TEST_BATCH_PREDICTION_NAME)
+        query_job = bp.iter_outputs()
+
+        bq_query_mock.assert_called_once_with(_TEST_ITER_DIRS_BQ_QUERY)
+
+        assert query_job.query == _TEST_ITER_DIRS_BQ_QUERY
+        assert query_job.job_id == _TEST_BQ_JOB_ID
+
+    def test_batch_prediction_iter_dirs_while_running(
+        self, get_batch_prediction_job_running_bq_output_mock
+    ):
+        """
+        Raises RuntimeError since outputs cannot be read while BatchPredictionJob is still running
+        """
+        with pytest.raises(RuntimeError):
+            bp = BatchPredictionJob(
+                batch_prediction_job_name=_TEST_BATCH_PREDICTION_NAME
+            )
+            bp.iter_outputs()
+
+    def test_batch_prediction_iter_dirs_invalid_output_info(
+        self, get_batch_prediction_job_empty_output_mock
+    ):
+        """
+        Raises NotImplementedError since the BatchPredictionJob's output_info
+        contains no output GCS directory or BQ dataset.
+        """
+        with pytest.raises(NotImplementedError):
+            bp = BatchPredictionJob(
+                batch_prediction_job_name=_TEST_BATCH_PREDICTION_NAME
+            )
+            bp.iter_outputs()
