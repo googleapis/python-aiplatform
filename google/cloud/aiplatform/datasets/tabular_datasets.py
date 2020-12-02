ad1166cbb48c27dfad8d32ba6fe27d4ecc613822
Morgan Du <morgandu@google.com>
2020-12-01 18:17:01 -0800

    feat: add tabular_dataset.py and test_tabular_dataset.py

---
+++ google/cloud/aiplatform/datasets/tabular_datasets.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+from typing import Optional, Sequence, Dict
+
+from google.auth import credentials as auth_credentials
+
+from google.cloud.aiplatform import schema
+from google.cloud.aiplatform.datasets import datasets
+
+class TabularDataset(datasets.Dataset):
+    """Managed tabular dataset resource for AI Platform"""
+
+    def __init__(
+        self,
+        dataset_name: str,
+        project: Optional[str] = None,
+        location: Optional[str] = None,
+        credentials: Optional[auth_credentials.Credentials] = None,
+    ):
+        """Retrieves an existing managed tabular dataset given a dataset name or ID.
+
+        Args:
+            dataset_name (str):
+                Required. A fully-qualified dataset resource name or dataset ID.
+                Example: "projects/123/locations/us-central1/datasets/456" or
+                "456" when project and location are initialized or passed.
+            project (str):
+                Optional project to retrieve dataset from. If not set, project
+                set in aiplatform.init will be used.
+            location (str):
+                Optional location to retrieve dataset from. If not set, location
+                set in aiplatform.init will be used.
+            credentials: Optional[auth_credentials.Credentials]=None,
+                Custom credentials to use to upload this model. Overrides
+                credentials set in aiplatform.init.
+        """
+
+        super().__init__(
+            dataset_name=dataset_name,
+            project=project,
+            location=location,
+            credentials=credentials
+        )
+
+        assert (self.metadata_schema_uri == schema.dataset.metadata.tabular), \
+            "%s is not a tabular dataset resource" % self.resource_name
+
+    @classmethod
+    def create(
+        cls,
+        display_name: str,
+        gcs_source: Optional[Sequence[str]] = None,
+        bq_source: Optional[str] = None,
+        labels: Optional[Dict] = None,
+        project: Optional[str] = None,
+        location: Optional[str] = None,
+        credentials: Optional[auth_credentials.Credentials] = None,
+    ) -> "TabularDataset":
+        """Creates a new tabular dataset.
+
+        Args:
+            display_name (str):
+                Required. The user-defined name of the Dataset.
+                The name can be up to 128 characters long and can be consist
+                of any UTF-8 characters.
+            gcs_source: Optional[Sequence[str]]=None:
+                Google Cloud Storage URI(-s) to the
+                input file(s). May contain wildcards. For more
+                information on wildcards, see
+                https://cloud.google.com/storage/docs/gsutil/addlhelp/WildcardNames.
+            bq_source: Optional[str]=None:
+                BigQuery URI to the input table.
+            labels: (Optional[Dict]) = None
+                The labels with user-defined metadata to organize your
+                Datasets.
+
+                Label keys and values can be no longer than 64 characters
+                (Unicode codepoints), can only contain lowercase letters,
+                numeric characters, underscores and dashes. International
+                characters are allowed. No more than 64 user labels can be
+                associated with one Dataset (System labels are excluded).
+
+                See https://goo.gl/xmQnxf for more information and examples
+                of labels. System reserved label keys are prefixed with
+                "aiplatform.googleapis.com/" and are immutable.
+            project: Optional[str]=None,
+                Project to upload this model to. Overrides project set in
+                aiplatform.init.
+            location: Optional[str]=None,
+                Location to upload this model to. Overrides location set in
+                aiplatform.init.
+            credentials: Optional[auth_credentials.Credentials]=None,
+                Custom credentials to use to upload this model. Overrides
+                credentials set in aiplatform.init.
+        Returns:
+            tabular_dataset (TabularDataset):
+                Instantiated representation of the managed tabular dataset resource.
+        """
+
+        metadata_schema_uri = schema.dataset.metadata.tabular
+
+        dataset_obj = super().create(
+            display_name=display_name,
+            metadata_schema_uri=metadata_schema_uri,
+            gcs_source=gcs_source,
+            bq_source=bq_source,
+            labels=labels,
+            project=project,
+            location=location,
+            credentials=credentials,
+        )
+
+        return dataset_obj
+
+    @property
+    def import_data(self):
+        raise AttributeError("'TabularDataset' object has no 'import_data' method")
+
+
---
+++ tests/unit/aiplatform/test_tabular_datasets.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import pytest
+
+from unittest import mock
+from importlib import reload
+from unittest.mock import patch
+
+from google.api_core import operation
+
+from google.cloud import aiplatform
+from google.cloud.aiplatform import TabularDataset
+from google.cloud.aiplatform import initializer
+from google.cloud.aiplatform import schema
+
+from google.cloud.aiplatform_v1beta1 import DatasetServiceClient
+from google.cloud.aiplatform_v1beta1 import Dataset as GapicDataset
+
+_TEST_PROJECT = "test-project"
+_TEST_LOCATION = "us-central1"
+_TEST_ID = "1028944691210842416"
+_TEST_PARENT = f"projects/{_TEST_PROJECT}/locations/{_TEST_LOCATION}"
+_TEST_NAME = f"projects/{_TEST_PROJECT}/locations/{_TEST_LOCATION}/datasets/{_TEST_ID}"
+
+_TEST_METADATA_SCHEMA_URI_TABULAR = schema.dataset.metadata.tabular
+_TEST_METADATA_SCHEMA_URI_NONTABULAR = schema.dataset.metadata.image
+
+_TEST_SOURCE_URI_GCS = "gs://my-bucket/my_index_file.jsonl"
+
+_TEST_SOURCE_URI_BQ = "bigquery://my-project/my-dataset"
+
+_TEST_LABEL = {"team": "experimentation", "trial_id": "x435"}
+_TEST_DISPLAY_NAME = "my_dataset_1234"
+_TEST_METADATA_TABULAR_GCS = {
+    "input_config": {"gcs_source": {"uri": [_TEST_SOURCE_URI_GCS]}}
+}
+_TEST_METADATA_TABULAR_BQ = {
+    "input_config": {"bigquery_source": {"uri": _TEST_SOURCE_URI_BQ}}
+}
+
+class TestTabularDataset:
+
+    def setup_method(self):
+        reload(initializer)
+        reload(aiplatform)
+
+    @pytest.fixture
+    def get_tabular_dataset_mock(self):
+        with patch.object(DatasetServiceClient, "get_dataset") as get_dataset_mock:
+            get_dataset_mock.return_value = GapicDataset(
+                display_name=_TEST_DISPLAY_NAME,
+                metadata_schema_uri=_TEST_METADATA_SCHEMA_URI_TABULAR,
+                labels=_TEST_LABEL,
+                name=_TEST_NAME,
+            )
+            yield get_dataset_mock
+
+    @pytest.fixture
+    def get_nontabular_dataset_mock(self):
+        with patch.object(DatasetServiceClient, "get_dataset") as get_dataset_mock:
+            get_dataset_mock.return_value = GapicDataset(
+                display_name=_TEST_DISPLAY_NAME,
+                metadata_schema_uri=_TEST_METADATA_SCHEMA_URI_NONTABULAR,
+                labels=_TEST_LABEL,
+                name=_TEST_NAME,
+            )
+            yield get_dataset_mock
+
+    @pytest.fixture
+    def create_dataset_mock(self):
+        with patch.object(
+            DatasetServiceClient, "create_dataset"
+        ) as create_dataset_mock:
+            create_dataset_lro_mock = mock.Mock(operation.Operation)
+            create_dataset_lro_mock.result.return_value = GapicDataset(
+                display_name=_TEST_DISPLAY_NAME,
+                name=_TEST_NAME,
+            )
+            create_dataset_mock.return_value = create_dataset_lro_mock
+            yield create_dataset_mock
+
+    @pytest.mark.usefixtures("get_tabular_dataset_mock")
+    def test_init_tabular_dataset(self, get_tabular_dataset_mock):
+        aiplatform.init(project=_TEST_PROJECT)
+        TabularDataset(dataset_name=_TEST_NAME)
+        get_tabular_dataset_mock.assert_called_once_with(name=_TEST_NAME)
+
+    @pytest.mark.usefixtures("get_nontabular_dataset_mock")
+    def test_init_nontabular_dataset(self):
+        aiplatform.init(project=_TEST_PROJECT)
+        with pytest.raises(AssertionError):
+            TabularDataset(dataset_name=_TEST_NAME)
+
+    @pytest.mark.usefixtures("get_tabular_dataset_mock")
+    def test_create_dataset_tabular(self, create_dataset_mock):
+        aiplatform.init(project=_TEST_PROJECT)
+
+        TabularDataset.create(
+            display_name=_TEST_DISPLAY_NAME,
+            bq_source=_TEST_SOURCE_URI_BQ,
+            labels=_TEST_LABEL,
+        )
+
+        expected_dataset = GapicDataset(
+            display_name=_TEST_DISPLAY_NAME,
+            metadata_schema_uri=_TEST_METADATA_SCHEMA_URI_TABULAR,
+            labels=_TEST_LABEL,
+            metadata=_TEST_METADATA_TABULAR_BQ,
+        )
+
+        create_dataset_mock.assert_called_once_with(
+            parent=_TEST_PARENT, dataset=expected_dataset
+        )
+
+    @pytest.mark.usefixtures("get_tabular_dataset_mock")
+    def test_no_import_data_method(self):
+        aiplatform.init(project=_TEST_PROJECT)
+        my_dataset = TabularDataset(dataset_name=_TEST_NAME)
+        with pytest.raises(AttributeError):
+            my_dataset.import_data
