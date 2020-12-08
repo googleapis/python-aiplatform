0fee278297743a752b0df510bf1c6f1c3c3fe217
Ivan Cheung <ivanmkc@google.com>
2020-12-09 00:46:41 +0900

    Added param handlers

--- samples/snippets/predict_tabular_forecasting_sample.py
+++ samples/snippets/predict_tabular_forecasting_sample.py
@@ -13,20 +13,28 @@
 # limitations under the License.
 
 # [START aiplatform_predict_tabular_forecasting_sample]
+from typing import Dict
+
 from google.cloud import aiplatform
+from google.protobuf import json_format
 from google.protobuf.struct_pb2 import Value
 
 
 def predict_tabular_forecasting_sample(
     endpoint: str,
-    instances: typing.Sequence[google.protobuf.struct_pb2.Value],
-    parameters: google.protobuf.struct_pb2.Value,
+    instance_dict: Dict,
     api_endpoint: str = "us-central1-aiplatform.googleapis.com",
 ):
     client_options = {"api_endpoint": api_endpoint}
     # Initialize client that will be used to create and send requests.
     # This client only needs to be created once, and can be reused for multiple requests.
     client = aiplatform.gapic.PredictionServiceClient(client_options=client_options)
+    # for more info on the instance schema, please use get_model_sample.py
+    # and look at the yaml found in instance_schema_uri
+    instance = json_format.ParseDict(instance_dict, Value())
+    instances = [instance]
+    parameters_dict = {}
+    parameters = json_format.ParseDict(parameters_dict, Value())
     response = client.predict(
         endpoint=endpoint, instances=instances, parameters=parameters
     )
