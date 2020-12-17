16423c7b81652ff4c2e1a9555817d15430080c22
Yu-Han Liu <yuhanliu@google.com>
2020-12-16 23:37:15 -0800

    0.4.0a1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 import setuptools  # type: ignore
 
 name = "google-cloud-aiplatform"
-version = "0.3.1-alpha"
+version = "0.4.0a1"
 description = "Cloud AI Platform API client library"
 
 package_root = os.path.abspath(os.path.dirname(__file__))
