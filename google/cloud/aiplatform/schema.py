c8347c04627c5d5207cb4fc3e41cb4a0d7e78449
Vinny Senthil <vinnysenthil@gmail.com>
2020-12-23 21:45:39 -0800

    Add AutoMLImageTrainingJob, tests, constants

--- google/cloud/aiplatform/schema.py
+++ google/cloud/aiplatform/schema.py
@@ -22,6 +22,8 @@ class training_job:
     class definition:
         custom_task = "gs://google-cloud-aiplatform/schema/trainingjob/definition/custom_task_1.0.0.yaml"
         tabular_task = "gs://google-cloud-aiplatform/schema/trainingjob/definition/automl_tabular_1.0.0.yaml"
+        automl_image_classification_task = "gs://google-cloud-aiplatform/schema/trainingjob/definition/automl_image_classification_1.0.0.yaml"
+        automl_image_object_detection_task = "gs://google-cloud-aiplatform/schema/trainingjob/definition/automl_image_object_detection_1.0.0.yaml"
 
 
 class dataset:
