d56b6958703f53bf64c72d277a49f4a1fd04e46c
Eric Schmidt <erschmid@google.com>
2021-02-12 11:13:25 -0800

    fix: sample failures due to v1beta1 features

--- samples/snippets/create_batch_prediction_job_bigquery_sample.py
+++ samples/snippets/create_batch_prediction_job_bigquery_sample.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # [START aiplatform_create_batch_prediction_job_bigquery_sample]
-from google.cloud import aiplatform_v1beta1 as aiplatform
+from google.cloud import aiplatform_v1beta1
 from google.protobuf import json_format
 from google.protobuf.struct_pb2 import Value
 
@@ -33,7 +33,7 @@ def create_batch_prediction_job_bigquery_sample(
     client_options = {"api_endpoint": api_endpoint}
     # Initialize client that will be used to create and send requests.
     # This client only needs to be created once, and can be reused for multiple requests.
-    client = aiplatform.gapic.JobServiceClient(client_options=client_options)
+    client = aiplatform_v1beta1.JobServiceClient(client_options=client_options)
     model_parameters_dict = {}
     model_parameters = json_format.ParseDict(model_parameters_dict, Value())
 
