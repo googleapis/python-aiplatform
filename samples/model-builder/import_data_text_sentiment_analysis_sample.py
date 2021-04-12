600a43905be8deb0f931865b37f9926b711168b0
Vinny Senthil <vinnysenthil@gmail.com>
2021-04-12 15:42:34 -0600

    Remove reference to ipdb

--- samples/model-builder/import_data_text_sentiment_analysis_sample.py
+++ samples/model-builder/import_data_text_sentiment_analysis_sample.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ipdb import set_trace
-
 from typing import List, Union
 
 from google.cloud import aiplatform
