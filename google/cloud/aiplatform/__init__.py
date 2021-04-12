d39fd6c47f98ac966d125986c4870815b923363e
nachocano <nachoacano@gmail.com>
2021-04-12 13:47:58 -0700

    feat: re-enabling singular log methods

--- google/cloud/aiplatform/__init__.py
+++ google/cloud/aiplatform/__init__.py
@@ -48,9 +48,9 @@ aiplatform.init(project='my_project')
 """
 init = initializer.global_config.init
 
-# log_param = metadata.metadata_service.log_param
+log_param = metadata.metadata_service.log_param
 log_params = metadata.metadata_service.log_params
-# log_metric = metadata.metadata_service.log_metric
+log_metric = metadata.metadata_service.log_metric
 log_metrics = metadata.metadata_service.log_metrics
 set_experiment = metadata.metadata_service.set_experiment
 get_experiment = metadata.metadata_service.get_experiment
@@ -61,14 +61,13 @@ __all__ = (
     "explain",
     "gapic",
     "init",
-    # "log_param",
+    "log_param",
     "log_params",
-    # "log_metric",
+    "log_metric",
     "log_metrics",
     "get_experiment",
     "set_experiment",
     "set_run",
-    "get_pipeline",
     "AutoMLImageTrainingJob",
     "AutoMLTabularTrainingJob",
     "AutoMLTextTrainingJob",
