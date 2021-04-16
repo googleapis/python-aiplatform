05f296ca7ec0ac87c02b0af4d891aa05af02166a
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-16 00:44:31 -0400

    chore: update workaround to make sic work

--- setup.py
+++ setup.py
@@ -20,6 +20,21 @@ import os
 
 import setuptools  # type: ignore
 
+# Disable version normalization performed by setuptools.setup()
+try:
+    # Try the approach of using sic(), added in setuptools 46.1.0
+    from setuptools import sic
+except ImportError:
+    # Try the approach of replacing packaging.version.Version
+    sic = lambda v: v
+    try:
+        # setuptools >=39.0.0 uses packaging from setuptools.extern
+        from setuptools.extern import packaging
+    except ImportError:
+        # setuptools <39.0.0 uses packaging from pkg_resources.extern
+        from pkg_resources.extern import packaging
+    packaging.version.Version = packaging.version.LegacyVersion
+
 name = "google-cloud-aiplatform"
 version = "0.7.0"
 description = "Cloud AI Platform API client library"
@@ -31,7 +46,7 @@ with io.open(readme_filename, encoding="utf-8") as readme_file:
 
 setuptools.setup(
     name=name,
-    version=setuptools.sic(version),
+    version=sic(version),
     description=description,
     long_description=readme,
     packages=setuptools.PEP420PackageFinder.find(),
