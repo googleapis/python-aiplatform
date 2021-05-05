9ae2c6e451a6a8114778d0a8629bc69d49a8b25f
Ivan Cheung <ivans.mailbox@gmail.com>
2021-05-05 19:34:41 -0400

    Bump google-cloud-storage min version to 1.32.0
    This PR (https://github.com/googleapis/python-aiplatform/pull/357) introduced a mock that uses google.cloud.storage.blob.Blob.download_as_bytes.
    
    This method was introduced in 1.32.0.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setuptools.setup(
     install_requires=(
         "google-api-core[grpc] >= 1.22.2, < 2.0.0dev",
         "proto-plus >= 1.10.1",
-        "google-cloud-storage >= 1.26.0, < 2.0.0dev",
+        "google-cloud-storage >= 1.32.0, < 2.0.0dev",
         "google-cloud-bigquery >= 1.15.0, < 3.0.0dev",
     ),
     python_requires=">=3.6",
