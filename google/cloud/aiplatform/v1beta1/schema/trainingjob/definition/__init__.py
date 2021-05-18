46f746ee211ad89503f8f570a8b52079f8added7
yoshi-automation <yoshi-automation@google.com>
2021-05-18 05:49:26 +0000

    chore: updates prediction and training proto files to v1beta1 AI Platform client libraries. feat!: removes unused protos from schema/ folders: schema/io_format.proto, schema/saved_query_metadata.proto feat!: ImageSegmentationPredictionResult.category_mask field changed to string data type feat: AutoMlImageSegmentationInputs.ModelType adds MOBILE_TF_LOW_LATENCY constant feat: AutoMlVideoClassificationInputs.ModelType adds MOBILE_JETSON_VERSATILE_1 constant
    Committer: @telpirion
    PiperOrigin-RevId: 357223795
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Fri Feb 12 10:50:54 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: f9cd594c912a2ff456cc2b33f54dcc238133bb0c
    Source-Link: https://github.com/googleapis/googleapis/commit/f9cd594c912a2ff456cc2b33f54dcc238133bb0c

--- google/cloud/aiplatform/v1beta1/schema/trainingjob/definition/__init__.py
+++ google/cloud/aiplatform/v1beta1/schema/trainingjob/definition/__init__.py
@@ -15,15 +15,6 @@
 # limitations under the License.
 #
 
-from google.cloud.aiplatform.v1beta1.schema.trainingjob.definition_v1beta1.types.automl_forecasting import (
-    AutoMlForecasting,
-)
-from google.cloud.aiplatform.v1beta1.schema.trainingjob.definition_v1beta1.types.automl_forecasting import (
-    AutoMlForecastingInputs,
-)
-from google.cloud.aiplatform.v1beta1.schema.trainingjob.definition_v1beta1.types.automl_forecasting import (
-    AutoMlForecastingMetadata,
-)
 from google.cloud.aiplatform.v1beta1.schema.trainingjob.definition_v1beta1.types.automl_image_classification import (
     AutoMlImageClassification,
 )
@@ -77,6 +68,15 @@ from google.cloud.aiplatform.v1beta1.schema.trainingjob.definition_v1beta1.types
 )
 from google.cloud.aiplatform.v1beta1.schema.trainingjob.definition_v1beta1.types.automl_text_sentiment import (
     AutoMlTextSentimentInputs,
+)
+from google.cloud.aiplatform.v1beta1.schema.trainingjob.definition_v1beta1.types.automl_time_series_forecasting import (
+    AutoMlForecasting,
+)
+from google.cloud.aiplatform.v1beta1.schema.trainingjob.definition_v1beta1.types.automl_time_series_forecasting import (
+    AutoMlForecastingInputs,
+)
+from google.cloud.aiplatform.v1beta1.schema.trainingjob.definition_v1beta1.types.automl_time_series_forecasting import (
+    AutoMlForecastingMetadata,
 )
 from google.cloud.aiplatform.v1beta1.schema.trainingjob.definition_v1beta1.types.automl_video_action_recognition import (
     AutoMlVideoActionRecognition,
