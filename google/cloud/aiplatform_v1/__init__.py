d71ee47452192e05aabdaf67712e2956511f8a43
yoshi-automation <yoshi-automation@google.com>
2021-05-30 08:33:00 +0000

    chore: upgrade gapic-generator-python to 0.46.1
    PiperOrigin-RevId: 373400747
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed May 12 10:34:35 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 162641cfe5573c648df679a6dd30385650a08704
    Source-Link: https://github.com/googleapis/googleapis/commit/162641cfe5573c648df679a6dd30385650a08704

--- google/cloud/aiplatform_v1/__init__.py
+++ google/cloud/aiplatform_v1/__init__.py
@@ -187,6 +187,14 @@ from .types.training_pipeline import TrainingPipeline
 from .types.user_action_reference import UserActionReference
 
 __all__ = (
+    "DatasetServiceAsyncClient",
+    "EndpointServiceAsyncClient",
+    "JobServiceAsyncClient",
+    "MigrationServiceAsyncClient",
+    "ModelServiceAsyncClient",
+    "PipelineServiceAsyncClient",
+    "PredictionServiceAsyncClient",
+    "SpecialistPoolServiceAsyncClient",
     "AcceleratorType",
     "ActiveLearningConfig",
     "Annotation",
