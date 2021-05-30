0f9ce8371583299c9948ee685e7fe4981b405a22
yoshi-automation <yoshi-automation@google.com>
2021-05-30 08:40:06 +0000

    chore: restores missing BUILD.bazel build rules
    Committer: @telpirion
    PiperOrigin-RevId: 373615236
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu May 13 11:08:54 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 556c373cd2706e54a2881235beddd3f47638fe64
    Source-Link: https://github.com/googleapis/googleapis/commit/556c373cd2706e54a2881235beddd3f47638fe64

--- google/cloud/aiplatform/v1beta1/schema/predict/prediction/__init__.py
+++ google/cloud/aiplatform/v1beta1/schema/predict/prediction/__init__.py
@@ -36,6 +36,9 @@ from google.cloud.aiplatform.v1beta1.schema.predict.prediction_v1beta1.types.tex
 from google.cloud.aiplatform.v1beta1.schema.predict.prediction_v1beta1.types.text_sentiment import (
     TextSentimentPredictionResult,
 )
+from google.cloud.aiplatform.v1beta1.schema.predict.prediction_v1beta1.types.time_series_forecasting import (
+    TimeSeriesForecastingPredictionResult,
+)
 from google.cloud.aiplatform.v1beta1.schema.predict.prediction_v1beta1.types.video_action_recognition import (
     VideoActionRecognitionPredictionResult,
 )
@@ -54,6 +57,7 @@ __all__ = (
     "TabularRegressionPredictionResult",
     "TextExtractionPredictionResult",
     "TextSentimentPredictionResult",
+    "TimeSeriesForecastingPredictionResult",
     "VideoActionRecognitionPredictionResult",
     "VideoClassificationPredictionResult",
     "VideoObjectTrackingPredictionResult",
