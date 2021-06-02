669071e8952da0a86723237193405a0f2a4ba379
yoshi-automation <yoshi-automation@google.com>
2021-06-02 06:46:36 +0000

    chore: update gapic-generator-python to 0.40.11
    PiperOrigin-RevId: 359562873
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Feb 25 10:52:32 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 07932bb995e7dc91b43620ea8402c6668c7d102c
    Source-Link: https://github.com/googleapis/googleapis/commit/07932bb995e7dc91b43620ea8402c6668c7d102c

--- google/cloud/aiplatform_v1/services/model_service/async_client.py
+++ google/cloud/aiplatform_v1/services/model_service/async_client.py
@@ -101,8 +101,36 @@ class ModelServiceAsyncClient:
         ModelServiceClient.parse_common_location_path
     )
 
-    from_service_account_info = ModelServiceClient.from_service_account_info
-    from_service_account_file = ModelServiceClient.from_service_account_file
+    @classmethod
+    def from_service_account_info(cls, info: dict, *args, **kwargs):
+        """Creates an instance of this client using the provided credentials info.
+
+        Args:
+            info (dict): The service account private key info.
+            args: Additional arguments to pass to the constructor.
+            kwargs: Additional arguments to pass to the constructor.
+
+        Returns:
+            ModelServiceAsyncClient: The constructed client.
+        """
+        return ModelServiceClient.from_service_account_info.__func__(ModelServiceAsyncClient, info, *args, **kwargs)  # type: ignore
+
+    @classmethod
+    def from_service_account_file(cls, filename: str, *args, **kwargs):
+        """Creates an instance of this client using the provided credentials
+        file.
+
+        Args:
+            filename (str): The path to the service account private key json
+                file.
+            args: Additional arguments to pass to the constructor.
+            kwargs: Additional arguments to pass to the constructor.
+
+        Returns:
+            ModelServiceAsyncClient: The constructed client.
+        """
+        return ModelServiceClient.from_service_account_file.__func__(ModelServiceAsyncClient, filename, *args, **kwargs)  # type: ignore
+
     from_service_account_json = from_service_account_file
 
     @property
