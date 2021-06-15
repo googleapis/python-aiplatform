3e6c94063ff46430504e5ca1f48c0211960e695d
sirtorry <torryyang@google.com>
2021-06-15 16:38:47 -0400

    add column to init

--- google/cloud/aiplatform/__init__.py
+++ google/cloud/aiplatform/__init__.py
@@ -45,6 +45,7 @@ from google.cloud.aiplatform.training_jobs import (
     AutoMLTextTrainingJob,
     AutoMLVideoTrainingJob,
 )
+from google.cloud.aiplatform.column import data_types
 
 """
 Usage:
