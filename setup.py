c253fa62cc457ae5b9585e03460db7152111327d
Bu Sun Kim <busunkim@google.com>
2021-06-22 23:11:18 +0000

    fix: fix typo in setup.py

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setuptools.setup(
     platforms="Posix; MacOS X; Windows",
     include_package_data=True,
     install_requires=(
-        "google-api-core[grpc] >= 1.26.0, <2.0.0dev","
+        "google-api-core[grpc] >= 1.26.0, <2.0.0dev",
         "proto-plus >= 1.10.1",
         "packaging >= 14.3",
         "google-cloud-storage >= 1.32.0, < 2.0.0dev",
