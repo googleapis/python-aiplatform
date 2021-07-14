9881744de41fc004ee8ba7dc2daa57660fd9aa3e
anooshree <alsy2k@gmail.com>
2021-07-14 17:34:40 -0400

    File restructuring based on meeting notes

--- google/cloud/aiplatform/experimental/vertex_model/base.py
+++ google/cloud/aiplatform/experimental/vertex_model/base.py
@@ -38,132 +38,78 @@ from typing import (
 )
 
 import proto
+import torch 
 
 from google.api_core import operation
 from google.auth import credentials as auth_credentials
 from google.cloud.aiplatform import initializer
 from google.cloud.aiplatform import utils
 from google.cloud.aiplatform.compat.types import encryption_spec as gca_encryption_spec
+from google.cloud import aiplatform
+                         
+from torch.utils.data import Dataset, Dataloader
+from . import serializers  
+
+# Wrapper function to handle cloud training extension of user code
+def vertex_function_wrapper(method, training_mode):
+
+    # Check we are using the subclass definition
+    print(method.__self__.__class__)
+
+    if training_mode == 'local':
+        return method
+
+    def f(*args, **kwargs):
+        dataset = kwargs['dataset']
+
+        serializer = 
+            method.__self__.__class__._data_serialization_mapping[type(dataset)][1]
+        serializer(dataset, staging_bucket + 'dataset.csv', args[1], '~/temp_dir', 'training')
+
+        """            
+        # Edit run of job here
+        job = aiplatform.CustomTrainingJob(...)
+        job.run(
+            args = ['dataset',  staging_bucket + 'dataset.csv',
+                    'dataset_type', dataset] 
+        ) 
+        """
+
+    def p(*args, **kwargs):
+        pass
+
+    def bp(*args, **kwargs):
+        pass
+    
+    def e(*args, **kwargs):
+        pass
+
+    if method.__name__ == 'fit':
+        return f
+    elif method.__name__ == 'predict':
+        return p
+    elif method.__name__ == 'batch_predict':
+        return bp
+    else:
+        return e
 
 
 class VertexModel:
 
     _data_serialization_mapping = {
-        pd.DataFrame : (deserialize_data_in_memory, serialize_data_in_memory)
+        pd.DataFrame : (deserialize_data_in_memory, serialize_data_in_memory).
+        DataLoader: (deserialize_dataloader, serialize_dataloader)
     }
-
-    # Default to local training on creation, at least for this prototype.
-    self.training_mode = 'local'
 
     """ Parent class that users can extend to use the Vertex AI SDK """
     def __init__(self):
-        if self.training_mode == 'cloud':
-            self.fit = vertex_function_wrapper(self.fit)
-            self.predict = vertex_function_wrapper(self.predict)
-            self.batch_predict = vertex_function_wrapper(self.batch_predict)
-            self.eval = vertex_function_wrapper(self.eval)
+        # Default to local training on creation, at least for this prototype.
+        self.training_mode = 'local'
 
-    def serialize_data_in_memory(self, artifact_uri, obj: pd.Dataframe, temp_dir: str, dataset_type: str):
-        """ Provides out-of-the-box serialization for input """
-
-        # Designate csv path and write the pandas DataFrame to the path
-        # Convention: file name is my_training_dataset, my_test_dataset, etc.
-        path_to_csv = temp_dir + "/" + "my_" + dataset_type + "_dataset.csv"
-        obj.to_csv(path_to_csv)
-
-        gcs_bucket, gcs_blob_prefix = extract_bucket_and_prefix_from_gcs_path(artifact_uri)
-
-        local_file_name = pathlib.Path(path_to_csv).name
-        blob_path = local_file_name
-
-        if gcs_blob_prefix:
-            blob_path = "/".join([gcs_blob_prefix, blob_path])
-
-        client = storage.Client(project=initializer.global_config.project, 
-                                credentials=initializer.global_config.credentials))
-
-        bucket = client.bucket(gcs_bucket)
-        blob = bucket.blob(blob_path)
-        blob.upload_from_filename(path_to_csv)
-
-        gcs_path = "".join(["gs://", "/".join([blob.bucket.name, blob.name])])
-        return gcs_path
-
-    def deserialize_data_in_memory(cls, artifact_uri):
-        """ Provides out-of-the-box deserialization after training and prediction is complete """
-        
-        gcs_bucket, gcs_blob = utils.extract_bucket_and_prefix_from_gcs_path(
-            artifact_uri
-        )
-
-        client = storage.Client(project=initializer.global_config.project, 
-                                credentials=initializer.global_config.credentials)
-        bucket = client.bucket(gcs_bucket)
-        blob = bucket.blob(gcs_blob)
-
-        # Incrementally download the CSV file until the header is retrieved
-        first_new_line_index = -1
-        start_index = 0
-        increment = 1000
-        line = ""
-
-        try:
-            logger = logging.getLogger("google.resumable_media._helpers")
-            logging_warning_filter = utils.LoggingFilter(logging.INFO)
-            logger.addFilter(logging_warning_filter)
-
-            while first_new_line_index == -1:
-                line += blob.download_as_bytes(
-                    start=start_index, end=start_index + increment
-                ).decode("utf-8")
-
-                first_new_line_index = line.find("\n")
-                start_index += increment
-
-            header_line = line[:first_new_line_index]
-
-            # Split to make it an iterable
-            header_line = header_line.split("\n")[:1]
-
-            csv_reader = csv.reader(header_line, delimiter=",")
-        except (ValueError, RuntimeError) as err:
-            raise RuntimeError(
-                "There was a problem extracting the headers from the CSV file at '{}': {}".format(
-                    gcs_csv_file_path, err
-                )
-            )
-        finally:
-            logger.removeFilter(logging_warning_filter)
-
-        # Return a pandas DataFrame read from the csv in the cloud
-        return pandas.read_csv(next(csv_reader))
-
-    def serialize_remote_dataloader:
-        # writes the referenced data to the run-time bucket
-        pass
-
-    def deserialize_remote_dataloader:
-        # read the data from a run-time bucket 
-        # and reformat to a DataLoader
-        pass
-
-    def serialize_local_dataloader:
-        # finds the local source, and copies 
-        # data to the user-designated staging bucket
-        pass
-
-    def deserialize_local_dataloader:
-        # read the data from user-designated staging bucket and
-        # reformat to a DataLoader
-        pass
-
-    def serialize_dataloader:
-        # introspect to determine which method is called
-        pass
-
-    def deserialize_dataloader:
-        # introspect to determine which method is called
-        pass
+        self.fit = vertex_function_wrapper(self.fit, self.training_mode)
+        self.predict = vertex_function_wrapper(self.predict, self.training_mode)
+        self.batch_predict = vertex_function_wrapper(self.batch_predict, self.training_mode)
+        self.eval = vertex_function_wrapper(self.eval, self.training_mode)
 
     @abc.abstractmethod
     def fit(self, data, epochs, learning_rate, dataset: pd.DataFrame):
@@ -184,31 +130,4 @@ class VertexModel:
     def eval(self, data):
         """ Evaluate model. """
         pass
-
-    def vertex_function_wrapper(method):
-
-        # how to determine function type within another function??
-        def f(*args, **kwargs):
-            dataset = kwargs['dataset']
-
-            serializer = fit_method.__self__.__class__._data_serialization_mapping[type(dataset)][1]
-            serializer(dataset, staging_bucket + 'dataset.csv', args[1], '~/temp_dir', 'training')
-
-            """            
-            # Edit run of job here?
-            job = aiplatform.CustomTrainingJob(...)
-            job.run(
-                args = ['dataset',  staging_bucket + 'dataset.csv',
-                        'dataset_type', dataset] 
-            ) 
-            """
-
-        if (fit_method.__name__ == 'fit'):
-            f(inspect.signature(fit_method))
-
-        # TODO: wrapper for predict and/or eval (could be the same function)
-
-        # def p(*args, **kwargs):
-        
-        
 
---
+++ google/cloud/aiplatform/experimental/vertex_model/serializers.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from concurrent import futures
+import datetime
+import functools
+import inspect
+import logging
+import pandas as pd
+import sys
+import threading
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    Iterable,
+    Optional,
+    Sequence,
+    Tuple,
+    Type,
+    Union,
+)
+
+import proto
+import torch 
+
+from google.api_core import operation
+from google.auth import credentials as auth_credentials
+from google.cloud.aiplatform import initializer
+from google.cloud.aiplatform import utils
+from google.cloud.aiplatform.compat.types import encryption_spec as gca_encryption_spec
+from google.cloud import aiplatform
+                         
+from torch.utils.data import Dataset, Dataloader
+
+def _serialize_data_in_memory(self, artifact_uri, obj: pd.Dataframe, 
+                                 temp_dir: str, dataset_type: str):
+        # Designate csv path and write the pandas DataFrame to the path
+        # Convention: file name is my_training_dataset, my_test_dataset, etc.
+        path_to_csv = temp_dir + "/" + "my_" + dataset_type + "_dataset.csv"
+        obj.to_csv(path_to_csv)
+
+        gcs_bucket, gcs_blob_prefix = extract_bucket_and_prefix_from_gcs_path(artifact_uri)
+
+        local_file_name = pathlib.Path(path_to_csv).name
+        blob_path = local_file_name
+
+        if gcs_blob_prefix:
+            blob_path = "/".join([gcs_blob_prefix, blob_path])
+
+        client = storage.Client(project=initializer.global_config.project, 
+                                credentials=initializer.global_config.credentials))
+
+        bucket = client.bucket(gcs_bucket)
+        blob = bucket.blob(blob_path)
+        blob.upload_from_filename(path_to_csv)
+
+        gcs_path = "".join(["gs://", "/".join([blob.bucket.name, blob.name])])
+        return gcs_path
+
+def _deserialize_data_in_memory(cls, artifact_uri):
+    """ Provides out-of-the-box deserialization after training and prediction is complete """
+    
+    gcs_bucket, gcs_blob = utils.extract_bucket_and_prefix_from_gcs_path(
+        artifact_uri
+    )
+
+    client = storage.Client(project=initializer.global_config.project, 
+                            credentials=initializer.global_config.credentials)
+    bucket = client.bucket(gcs_bucket)
+    blob = bucket.blob(gcs_blob)
+
+    # Incrementally download the CSV file until the header is retrieved
+    first_new_line_index = -1
+    start_index = 0
+    increment = 1000
+    line = ""
+
+    try:
+        logger = logging.getLogger("google.resumable_media._helpers")
+        logging_warning_filter = utils.LoggingFilter(logging.INFO)
+        logger.addFilter(logging_warning_filter)
+
+        while first_new_line_index == -1:
+            line += blob.download_as_bytes(
+                start=start_index, end=start_index + increment
+            ).decode("utf-8")
+
+            first_new_line_index = line.find("\n")
+            start_index += increment
+
+        header_line = line[:first_new_line_index]
+
+        # Split to make it an iterable
+        header_line = header_line.split("\n")[:1]
+
+        csv_reader = csv.reader(header_line, delimiter=",")
+    except (ValueError, RuntimeError) as err:
+        raise RuntimeError(
+            "There was a problem extracting the headers from the CSV file at '{}': {}".format(
+                gcs_csv_file_path, err
+            )
+        )
+    finally:
+        logger.removeFilter(logging_warning_filter)
+
+    # Return a pandas DataFrame read from the csv in the cloud
+    return pandas.read_csv(next(csv_reader))
+
+def _serialize_remote_dataloader:
+    # writes the referenced data to the run-time bucket
+    pass
+
+def _deserialize_remote_dataloader:
+    # read the data from a run-time bucket 
+    # and reformat to a DataLoader
+    pass
+
+def _serialize_local_dataloader:
+    # finds the local source, and copies 
+    # data to the user-designated staging bucket
+    pass
+
+def _deserialize_local_dataloader:
+    # read the data from user-designated staging bucket and
+    # reformat to a DataLoader
+    pass
+
+def _serialize_dataloader:
+    # introspect to determine which method is called
+    pass
+
+def _deserialize_dataloader:
+    # introspect to determine which method is called
+    pass
