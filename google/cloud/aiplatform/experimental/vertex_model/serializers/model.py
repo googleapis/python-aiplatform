7f33fa0c0d220fa5b38827090ac9dd04dca5d993
anooshree <alsy2k@gmail.com>
2021-07-29 13:31:11 -0400

    Setup for model serialization

---
+++ google/cloud/aiplatform/experimental/vertex_model/serializers/model.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import pathlib
+import tempfile
+import torch
+
+from torch.data.utils import DataLoader
+
+from google.cloud import storage
+from google.cloud.aiplatform import initializer
+from google.cloud.aiplatform import utils
+
+def _serialize_local_model():
+    raise NotImplementedError
+
+def _deserialize_remote_model():
+    raise NotImplementedError
