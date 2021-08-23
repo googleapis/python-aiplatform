c64c9e4414c38491d359e8af32616ef003f93d02
anooshree <alsy2k@gmail.com>
2021-08-23 14:32:48 -0400

    Moving around container script code

--- google/cloud/aiplatform/experimental/vertex_model/base.py
+++ google/cloud/aiplatform/experimental/vertex_model/base.py
@@ -61,11 +61,12 @@ COMMAND_STRING_CODE_SETUP = """
 from fastapi import FastAPI, Request
 from google.cloud.aiplatform.experimental.vertex_model.serializers import model
 
-my_model = model._deserialize_remote_model(os.environ['AIP_STORAGE_URI'] + '/my_local_model.pth')
-
 """
 
 COMMAND_STRING_CODE_APIS = """
+
+my_model = model._deserialize_remote_model(os.environ['AIP_STORAGE_URI'] + '/my_local_model.pth')
+
 @app.get(os.environ['AIP_HEALTH_ROUTE'], status_code=200)
 def health():
     return {}
