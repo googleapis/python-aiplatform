3683af671afc3a213c0758e04f8b246e6849ebb4
anooshree <alsy2k@gmail.com>
2021-08-24 10:41:18 -0400

    Changing pip install again

--- google/cloud/aiplatform/experimental/vertex_model/base.py
+++ google/cloud/aiplatform/experimental/vertex_model/base.py
@@ -50,8 +50,7 @@ except ImportError:
 
 COMMAND_STRING_CLI = """sh
 -c
-(PIP_DISABLE_PIP_VERSION_CHECK=1 python3 -m pip install --quiet --no-warn-script-location 'pandas' 'fastapi' 'torch' || PIP_DISABLE_PIP_VERSION_CHECK=1 python3 -m pip install --quiet --no-warn-script-location 'pandas' 'fastapi' 'torch' --user) && \"$0\" \"$@\"
-(PIP_DISABLE_PIP_VERSION_CHECK=1 python3 -m pip install --force-reinstall --upgrade 'google-cloud-aiplatform @ git+https://github.com/googleapis/python-aiplatform@refs/pull/628/head#egg=google-cloud-aiplatform' || PIP_DISABLE_PIP_VERSION_CHECK=1 python3 -m pip install --force-reinstall --upgrade 'google-cloud-aiplatform @ git+https://github.com/googleapis/python-aiplatform@refs/pull/628/head#egg=google-cloud-aiplatform' --user)
+python3 -m pip install --user --disable-pip-version-check --force-reinstall --upgrade 'fastapi' 'torch' 'pandas' 'google-cloud-aiplatform @ git+https://github.com/googleapis/python-aiplatform@refs/pull/628/head#egg=google-cloud-aiplatform' && \"$0\" \"$@\"
 sh
 -ec
 program_path=$(mktemp)\nprintf \"%s\" \"$0\" > \"$program_path\"\npython3 -u \"$program_path\" \"$@\"\n
