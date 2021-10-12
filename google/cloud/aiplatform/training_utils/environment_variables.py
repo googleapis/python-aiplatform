40c560d62fbd4d350af3e06239a6b7b06859c692
Michael Kovalski <mkovalski@google.com>
2021-10-12 14:29:00 +0000

    feat: Adding envrionment variables from dev branch and adding a few for tensorboard

---
+++ google/cloud/aiplatform/training_utils/environment_variables.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import json
+import os
+
+from typing import Dict, Optional
+
+
+class EnvironmentVariables:
+    """Passes on OS' environment variables"""
+
+    @property
+    def training_data_uri(self) -> Optional[str]:
+        """
+        Returns:
+            Cloud Storage URI of a directory intended for training data. None if
+            environment variable not set.
+        """
+        return os.environ.get("AIP_TRAINING_DATA_URI")
+
+    @property
+    def validation_data_uri(self) -> Optional[str]:
+        """
+        Returns:
+            Cloud Storage URI of a directory intended for validation data. None
+            if environment variable not set.
+        """
+        return os.environ.get("AIP_VALIDATION_DATA_URI")
+
+    @property
+    def test_data_uri(self) -> Optional[str]:
+        """
+        Returns:
+            Cloud Storage URI of a directory intended for test data. None if
+            environment variable not set.
+        """
+        return os.environ.get("AIP_TEST_DATA_URI")
+
+    @property
+    def model_dir(self) -> Optional[str]:
+        """
+        Returns:
+            Cloud Storage URI of a directory intended for saving model artefacts.
+            None if environment variable not set.
+        """
+        return os.environ.get("AIP_MODEL_DIR")
+
+    @property
+    def checkpoint_dir(self) -> Optional[str]:
+        """
+        Returns:
+            Cloud Storage URI of a directory intended for saving checkpoints.
+            None if environment variable not set.
+        """
+        return os.environ.get("AIP_CHECKPOINT_DIR")
+
+    @property
+    def tensorboard_log_dir(self) -> Optional[str]:
+        """
+        Returns:
+            Cloud Storage URI of a directory intended for saving TensorBoard logs.
+            None if environment variable not set.
+        """
+        return os.environ.get("AIP_TENSORBOARD_LOG_DIR")
+
+    @property
+    def cluster_spec(self) -> Optional[Dict]:
+        """
+        Returns:
+            json string as described in https://cloud.google.com/ai-platform-unified/docs/training/distributed-training#cluster-variables
+            None if environment variable not set.
+        """
+        cluster_spec_env = os.environ.get("CLUSTER_SPEC")
+        if cluster_spec_env is not None:
+            return json.loads(cluster_spec_env)
+        else:
+            return None
+
+    @property
+    def tf_config(self) -> Optional[Dict]:
+        """
+        Returns:
+            json string as described in https://cloud.google.com/ai-platform-unified/docs/training/distributed-training#tf-config
+            None if environment variable not set.
+        """
+        tf_config_env = os.environ.get("TF_CONFIG")
+        if tf_config_env is not None:
+            return json.loads(tf_config_env)
+        else:
+            return None
+
+    @property
+    def tf_profiler_port(self) -> Optional[str]:
+        """
+      Returns:
+          Profiler port used for capturing profiling samples,
+          None if environment variable is not set.
+      """
+        return os.environ.get("AIP_TF_PROFILER_PORT")
+
+    @property
+    def tensorboard_api_uri(self) -> Optional[str]:
+        """
+        Returns:
+            API URI used for the tensorboard uploader,
+            None if environment variable is not set.
+        """
+        return os.environ.get("AIP_TENSORBOARD_API_URI")
+
+    @property
+    def tensorboard_resource_name(self) -> Optional[str]:
+        """
+        Returns:
+            The name of the tensorboard resource, in the form:
+            `projects/{project_id}/locations/{location}/tensorboards/{tensorboard_name}`
+            None if environment variable is not set.
+        """
+
+        return os.environ.get("AIP_TENSORBOARD_RESOURCE_NAME")
+
+    @property
+    def cloud_ml_job_id(self) -> Optional[str]:
+        """
+        Returns:
+            The name given to the training job,
+            None if environment variable is not set.
+        """
+
+        return os.environ.get("CLOUD_ML_JOB_ID")
