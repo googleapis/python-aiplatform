6c7e1fa06915ec02bbe95d8f534b2a15b3ac323c
sasha-gitg <44654632+sasha-gitg@users.noreply.github.com>
2021-10-21 15:28:52 -0400

    chore: upgrade GAPIC Metadata from v1beta1 to v1 and add experiment logging integration test (#778)

--- tests/unit/aiplatform/test_metadata_resources.py
+++ tests/unit/aiplatform/test_metadata_resources.py
@@ -26,12 +26,12 @@ from google.cloud.aiplatform import initializer
 from google.cloud.aiplatform.metadata import artifact
 from google.cloud.aiplatform.metadata import context
 from google.cloud.aiplatform.metadata import execution
-from google.cloud.aiplatform_v1beta1 import AddContextArtifactsAndExecutionsResponse
-from google.cloud.aiplatform_v1beta1 import Artifact as GapicArtifact
-from google.cloud.aiplatform_v1beta1 import Context as GapicContext
-from google.cloud.aiplatform_v1beta1 import Execution as GapicExecution
-from google.cloud.aiplatform_v1beta1 import LineageSubgraph
-from google.cloud.aiplatform_v1beta1 import (
+from google.cloud.aiplatform_v1 import AddContextArtifactsAndExecutionsResponse
+from google.cloud.aiplatform_v1 import Artifact as GapicArtifact
+from google.cloud.aiplatform_v1 import Context as GapicContext
+from google.cloud.aiplatform_v1 import Execution as GapicExecution
+from google.cloud.aiplatform_v1 import LineageSubgraph
+from google.cloud.aiplatform_v1 import (
     MetadataServiceClient,
     AddExecutionEventsResponse,
     Event,
