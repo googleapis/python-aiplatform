81678b749b909c6c45bafc5ba9017550a2343de3
wanyixu199 <93347150+wanyixu199@users.noreply.github.com>
2021-10-28 15:09:04 -0700

    Update constants.py
    Add new regions of Vertex Tables.

--- google/cloud/aiplatform/constants.py
+++ google/cloud/aiplatform/constants.py
@@ -18,12 +18,15 @@
 DEFAULT_REGION = "us-central1"
 SUPPORTED_REGIONS = {
     "asia-east1",
+    "asia-east2",
     "asia-northeast1",
     "asia-northeast3",
+    "asia-south1",
     "asia-southeast1",
     "australia-southeast1",
     "europe-west1",
     "europe-west2",
+    "europe-west3",
     "europe-west4",
     "northamerica-northeast1",
     "us-central1",
