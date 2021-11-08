b326974e894f872e337068f77dbca84712328096
Anthonios Partheniou <partheniou@google.com>
2021-11-08 18:51:18 +0000

    fix: exclude support for python 3.10

--- owlbot.py
+++ owlbot.py
@@ -81,7 +81,11 @@ s.remove_staging_dirs()
 # Add templated files
 # ----------------------------------------------------------------------------
 
-templated_files = common.py_library(cov_level=99, microgenerator=True)
+templated_files = common.py_library(
+    cov_level=99,
+    system_test_python_versions=["3.8"],
+    unit_test_python_versions=["3.6", "3.7", "3.8", "3.9"],
+    microgenerator=True)
 python.py_samples(skip_readmes=True)
 s.move(
     templated_files,
--- setup.py
+++ setup.py
@@ -93,7 +93,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
