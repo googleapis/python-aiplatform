3633d650b5c6bd2493c0bcf6cbcebedda28d5bc7
Morgan Du <morgandu@google.com>
2021-12-13 16:32:17 -0800

    update test_get_timestamp_proto using utc timezone info for seconds conversion

--- tests/unit/aiplatform/test_utils.py
+++ tests/unit/aiplatform/test_utils.py
@@ -22,6 +22,7 @@ from uuid import uuid4
 from random import choice
 from random import randint
 from string import ascii_letters
+from decimal import Decimal
 
 from google.protobuf import timestamp_pb2
 
@@ -366,8 +367,28 @@ def test_client_w_override_select_version():
 @pytest.mark.parametrize(
     "year,month,day,hour,minute,second,microsecond,expected_seconds,expected_nanos",
     [
-        (2021, 12, 23, 23, 59, 59, 999999, 1640332799, 999999046),
-        (2013, 1, 1, 1, 1, 1, 000000, 1357030861, 0),
+        (
+            2021,
+            12,
+            23,
+            23,
+            59,
+            59,
+            999999,
+            1640303999,
+            int(str(Decimal(1640303999.999999)).split(".")[1][:9]),
+        ),
+        (
+            2013,
+            1,
+            1,
+            1,
+            1,
+            1,
+            199999,
+            1357002061,
+            int(str(Decimal(1357002061.199999)).split(".")[1][:9]),
+        ),
     ],
 )
 def test_get_timestamp_proto(
@@ -389,6 +410,7 @@ def test_get_timestamp_proto(
         minute=minute,
         second=second,
         microsecond=microsecond,
+        tzinfo=datetime.timezone.utc,
     )
     true_timestamp_proto = timestamp_pb2.Timestamp(
         seconds=expected_seconds, nanos=expected_nanos
