73b17d57127345b3113407e7bcf88671f9846763
Sasha Sobran <asobran@google.com>
2021-12-14 09:50:26 -0500

    checkpoint

--- google/cloud/aiplatform/tensorboard/tensorboard_resource.py
+++ google/cloud/aiplatform/tensorboard/tensorboard_resource.py
@@ -285,3 +285,281 @@ class Tensorboard(_TensorboardServiceResource):
         _LOGGER.log_action_completed_against_resource("tensorboard", "updated", self)
 
         return self
+
+
+class TensorboardExperiment(_TensorboardServiceResource):
+    """Managed tensorboard resource for Vertex AI."""
+
+    _resource_noun = "experiments"
+    _getter_method = "get_tensorboard_experiment"
+    _list_method = "list_tensorboard_experiments"
+    _delete_method = "delete_tensorboard_experiment"
+    _parse_resource_name_method = "parse_tensorboard_experiment_path"
+    _format_resource_name_method = "tensorboard_experiment_path"
+
+    def __init__(
+        self,
+        tensorboard_experiment_name: str,
+        tensorboard_id: Optional[str] = None,
+        project: Optional[str] = None,
+        location: Optional[str] = None,
+        credentials: Optional[auth_credentials.Credentials] = None,
+    ):
+        """Retrieves an existing tensorboard experiment given a tensorboard experiment name or ID.
+
+        Example Usage:
+
+            tb_exp = aiplatform.TensorboardExperiment(
+                tensorboard_experiment_name= "projects/123/locations/us-central1/tensorboards/456/experiments/678" 
+            )
+
+            tb_exp = aiplatform.TensorboardExperiment(
+                tensorboard_experiment_name= "678"
+                tensorboard_id = "456" 
+            )
+
+        Args:
+            tensorboard_experiment_name (str):
+                Required. A fully-qualified tensorboard experiment resource name or resource ID.
+                Example: "projects/123/locations/us-central1/tensorboards/456/experiments/678" or
+                "678" when tensorboard_id is passed and project and location are initialized or passed.
+            tensorboard_id (str):
+                Optional. A tensorboard resource ID.
+            project (str):
+                Optional. Project to retrieve tensorboard from. If not set, project
+                set in aiplatform.init will be used.
+            location (str):
+                Optional. Location to retrieve tensorboard from. If not set, location
+                set in aiplatform.init will be used.
+            credentials (auth_credentials.Credentials):
+                Optional. Custom credentials to use to retrieve this Tensorboard. Overrides
+                credentials set in aiplatform.init.
+        """
+
+        super().__init__(
+            project=project,
+            location=location,
+            credentials=credentials,
+            resource_name=tensorboard_experiment_name,
+        )
+        self._gca_resource = self._get_gca_resource(
+            resource_name=tensorboard_experiment_name,
+            parent_resource_name_fields={
+                Tensorboard._resource_noun: tensorboard_id
+            } if tensorboard_id else tensorboard_id)
+
+    @classmethod
+    def create(
+        cls,
+        tensorboard_id: str,
+        tensorboard_experiment_id: Optional[str] = None,
+        display_name: Optional[str] = None,
+        description: Optional[str] = None,
+        labels: Optional[Dict[str, str]] = None,
+        project: Optional[str] = None,
+        location: Optional[str] = None,
+        credentials: Optional[auth_credentials.Credentials] = None,
+        request_metadata: Optional[Sequence[Tuple[str, str]]] = (),
+        encryption_spec_key_name: Optional[str] = None,
+    ) -> "Tensorboard":
+        """Creates a new tensorboard.
+
+        Example Usage:
+
+            tb = aiplatform.TensorboardExperiment.create(
+                display_name='my display name',
+                tensorboard_id='456'
+                tensorboard_experiment_id='my-experiment'
+                description='my description',
+                labels={
+                    'key1': 'value1',
+                    'key2': 'value2'
+                }
+            )
+
+        Args:
+            display_name (str):
+                Required. The user-defined name of the Tensorboard Experiment.
+                The name can be up to 128 characters long and can be consist
+                of any UTF-8 characters.
+            tensorboard_id
+            description (str):
+                Optional. Description of this Tensorboard.
+            labels (Dict[str, str]):
+                Optional. Labels with user-defined metadata to organize your Tensorboards.
+                Label keys and values can be no longer than 64 characters
+                (Unicode codepoints), can only contain lowercase letters, numeric
+                characters, underscores and dashes. International characters are allowed.
+                No more than 64 user labels can be associated with one Tensorboard
+                (System labels are excluded).
+                See https://goo.gl/xmQnxf for more information and examples of labels.
+                System reserved label keys are prefixed with "aiplatform.googleapis.com/"
+                and are immutable.
+            project (str):
+                Optional. Project to upload this model to. Overrides project set in
+                aiplatform.init.
+            location (str):
+                Optional. Location to upload this model to. Overrides location set in
+                aiplatform.init.
+            credentials (auth_credentials.Credentials):
+                Optional. Custom credentials to use to upload this model. Overrides
+                credentials set in aiplatform.init.
+            request_metadata (Sequence[Tuple[str, str]]):
+                Optional. Strings which should be sent along with the request as metadata.
+            encryption_spec_key_name (str):
+                Optional. Cloud KMS resource identifier of the customer
+                managed encryption key used to protect the tensorboard. Has the
+                form:
+                ``projects/my-project/locations/my-region/keyRings/my-kr/cryptoKeys/my-key``.
+                The key needs to be in the same region as where the compute
+                resource is created.
+
+                If set, this Tensorboard and all sub-resources of this Tensorboard will be secured by this key.
+
+                Overrides encryption_spec_key_name set in aiplatform.init.
+
+        Returns:
+            tensorboard (Tensorboard):
+                Instantiated representation of the managed tensorboard resource.
+        """
+
+        utils.validate_display_name(display_name)
+        if labels:
+            utils.validate_labels(labels)
+
+        api_client = cls._instantiate_client(location=location, credentials=credentials)
+
+        parent = initializer.global_config.common_location_path(
+            project=project, location=location
+        )
+
+        encryption_spec = initializer.global_config.get_encryption_spec(
+            encryption_spec_key_name=encryption_spec_key_name
+        )
+
+        gapic_tensorboard = gca_tensorboard.Tensorboard(
+            display_name=display_name,
+            description=description,
+            labels=labels,
+            encryption_spec=encryption_spec,
+        )
+
+        create_tensorboard_lro = api_client.create_tensorboard(
+            parent=parent, tensorboard=gapic_tensorboard, metadata=request_metadata
+        )
+
+        _LOGGER.log_create_with_lro(cls, create_tensorboard_lro)
+
+        created_tensorboard = create_tensorboard_lro.result()
+
+        _LOGGER.log_create_complete(cls, created_tensorboard, "tb")
+
+        return cls(
+            tensorboard_name=created_tensorboard.name,
+            project=project or initializer.global_config.project,
+            location=location or initializer.global_config.location,
+            credentials=credentials,
+        )
+
+    def update(
+        self,
+        display_name: Optional[str] = None,
+        description: Optional[str] = None,
+        labels: Optional[Dict[str, str]] = None,
+        request_metadata: Optional[Sequence[Tuple[str, str]]] = (),
+        encryption_spec_key_name: Optional[str] = None,
+    ) -> "Tensorboard":
+        """Updates an existing tensorboard.
+
+        Example Usage:
+
+            tb = aiplatform.Tensorboard(tensorboard_name='123456')
+            tb.update(
+                display_name='update my display name',
+                description='update my description',
+            )
+
+        Args:
+            display_name (str):
+                Optional. User-defined name of the Tensorboard.
+                The name can be up to 128 characters long and can be consist
+                of any UTF-8 characters.
+            description (str):
+                Optional. Description of this Tensorboard.
+            labels (Dict[str, str]):
+                Optional. Labels with user-defined metadata to organize your Tensorboards.
+                Label keys and values can be no longer than 64 characters
+                (Unicode codepoints), can only contain lowercase letters, numeric
+                characters, underscores and dashes. International characters are allowed.
+                No more than 64 user labels can be associated with one Tensorboard
+                (System labels are excluded).
+                See https://goo.gl/xmQnxf for more information and examples of labels.
+                System reserved label keys are prefixed with "aiplatform.googleapis.com/"
+                and are immutable.
+            request_metadata (Sequence[Tuple[str, str]]):
+                Optional. Strings which should be sent along with the request as metadata.
+            encryption_spec_key_name (str):
+                Optional. Cloud KMS resource identifier of the customer
+                managed encryption key used to protect the tensorboard. Has the
+                form:
+                ``projects/my-project/locations/my-region/keyRings/my-kr/cryptoKeys/my-key``.
+                The key needs to be in the same region as where the compute
+                resource is created.
+
+                If set, this Tensorboard and all sub-resources of this Tensorboard will be secured by this key.
+
+                Overrides encryption_spec_key_name set in aiplatform.init.
+
+        Returns:
+            tensorboard (Tensorboard):
+                The managed tensorboard resource.
+        """
+        update_mask = list()
+
+        if display_name:
+            utils.validate_display_name(display_name)
+            update_mask.append("display_name")
+
+        if description:
+            update_mask.append("description")
+
+        if labels:
+            utils.validate_labels(labels)
+            update_mask.append("labels")
+
+        encryption_spec = None
+        if encryption_spec_key_name:
+            encryption_spec = initializer.global_config.get_encryption_spec(
+                encryption_spec_key_name=encryption_spec_key_name,
+            )
+            update_mask.append("encryption_spec")
+
+        update_mask = field_mask_pb2.FieldMask(paths=update_mask)
+
+        gapic_tensorboard = gca_tensorboard.Tensorboard(
+            name=self.resource_name,
+            display_name=display_name,
+            description=description,
+            labels=labels,
+            encryption_spec=encryption_spec,
+        )
+
+        _LOGGER.log_action_start_against_resource(
+            "Updating", "tensorboard", self,
+        )
+
+        update_tensorboard_lro = self.api_client.update_tensorboard(
+            tensorboard=gapic_tensorboard,
+            update_mask=update_mask,
+            metadata=request_metadata,
+        )
+
+        _LOGGER.log_action_started_against_resource_with_lro(
+            "Update", "tensorboard", self.__class__, update_tensorboard_lro
+        )
+
+        update_tensorboard_lro.result()
+
+        _LOGGER.log_action_completed_against_resource("tensorboard", "updated", self)
+
+        return self
