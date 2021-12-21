8fc413d2e96d368dc1f01b7dd71157a54ba901e5
Andrew Ferlitsch <aferlitsch@google.com>
2021-12-21 11:40:46 -0800

    Create predict_image_classification_sample.py

---
+++ samples/model-builder/predict_image_classification_sample.py
@@ -0,0 +1,56 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from typing import Dict, List
+
+from google.cloud import aiplatform
+
+import base64
+
+import tensorflow as tf
+
+
+#  [START aiplatform_sdk_predict_images_classification_sample]
+def predict_tabular_classification_sample(
+    project: str,
+    location: str,
+    endpoint_name: str,
+    images: List,
+):
+    '''
+    Args
+        project: Your project ID or project number.
+        location: Region where Endpoint is located. For example, 'us-central1'.
+        endpoint_name: A fully qualified endpoint name or endpoint ID. Example: "projects/123/locations/us-central1/endpoints/456" or
+               "456" when project and location are initialized or passed.
+        images: A list of one or more images to return a prediction for.
+    '''
+    aiplatform.init(project=project, location=location)
+
+    endpoint = aiplatform.Endpoint(endpoint_name)
+    
+    instances = []
+    for image in images:
+      with tf.io.gfile.GFile(image, "rb") as f:
+          content = f.read()
+      instances.append({"content": base64.b64encode(content).decode("utf-8")})
+
+    response = endpoint.predict(instances=instances)
+
+    for prediction_ in response.predictions:
+        print(prediction_)
+
+
+#  [END aiplatform_sdk_predict_image_classification_sample]
