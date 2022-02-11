1c341544e9bd94c6ff0ee41177565c8c078673a3
Anthonios Partheniou <partheniou@google.com>
2022-02-11 10:17:43 -0500

    fix(deps): allow google-cloud-storage < 3.0.0dev (#1008)

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ setuptools.setup(
         "google-api-core[grpc] >= 1.26.0, <3.0.0dev",
         "proto-plus >= 1.10.1",
         "packaging >= 14.3",
-        "google-cloud-storage >= 1.32.0, < 2.0.0dev",
+        "google-cloud-storage >= 1.32.0, < 3.0.0dev",
         "google-cloud-bigquery >= 1.15.0, < 3.0.0dev",
     ),
     extras_require={
