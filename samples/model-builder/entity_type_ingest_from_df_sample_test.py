24ece4d4991e0fb5bc0380f552209f902714609a
Morgan Du <morgandu@google.com>
2022-02-16 11:10:11 -0800

    samples: add feature store samples

---
+++ samples/model-builder/entity_type_ingest_from_df_sample_test.py
@@ -0,0 +1,33 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import entity_type_ingest_from_df_sample
+
+import test_constants as constants
+
+
+def test_image_dataset_create_classification_sample(
+    mock_sdk_init, mock_get_entity_type, mock_ingest_from_df
+):
+    entity_type_ingest_from_df_sample.entity_type_ingest_feature_values_from_df_with_feature_time_field_sample(
+        project=constants.PROJECT,
+        location=constants.LOCATION,
+    )
+
+    mock_sdk_init.assert_called_once_with(
+        project=constants.PROJECT, location=constants.LOCATION
+    )
+    mock_ingest_from_df.assert_called_once_with(
+    )
