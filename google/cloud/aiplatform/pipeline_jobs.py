799ebd358dc5689bd326d9f4b3f875ec57eaf32f
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-03-17 10:23:56 -0600

    read json and yaml using load_yaml

--- google/cloud/aiplatform/pipeline_jobs.py
+++ google/cloud/aiplatform/pipeline_jobs.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2021 Google LLC
+# Copyright 2021-2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ from google.auth import credentials as auth_credentials
 from google.cloud.aiplatform import base
 from google.cloud.aiplatform import initializer
 from google.cloud.aiplatform import utils
-from google.cloud.aiplatform.utils import json_utils
+from google.cloud.aiplatform.utils import yaml_utils
 from google.cloud.aiplatform.utils import pipeline_utils
 from google.protobuf import json_format
 
@@ -170,9 +170,12 @@ class PipelineJob(base.VertexAiStatefulResource):
         self._parent = initializer.global_config.common_location_path(
             project=project, location=location
         )
-        pipeline_json = json_utils.load_json(
+
+        # this loads both .yaml and .json files because YAML is a superset of JSON
+        pipeline_json = yaml_utils.load_yaml(
             template_path, self.project, self.credentials
         )
+
         # Pipeline_json can be either PipelineJob or PipelineSpec.
         if pipeline_json.get("pipelineSpec") is not None:
             pipeline_job = pipeline_json
--- google/cloud/aiplatform/utils/yaml_utils.py
+++ google/cloud/aiplatform/utils/yaml_utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2020 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,23 +15,23 @@
 # limitations under the License.
 #
 
-import json
 from typing import Any, Dict, Optional
 
+import yaml
 from google.auth import credentials as auth_credentials
 from google.cloud import storage
 
 
-def load_json(
+def load_yaml(
     path: str,
     project: Optional[str] = None,
     credentials: Optional[auth_credentials.Credentials] = None,
 ) -> Dict[str, Any]:
-    """Loads data from a JSON document.
+    """Loads data from a YAML document.
 
     Args:
       path (str):
-          Required. The path of the JSON document in Google Cloud Storage or
+          Required. The path of the YAML document in Google Cloud Storage or
           local.
       project (str):
           Optional. Project to initiate the Storage client with.
@@ -39,46 +39,46 @@ def load_json(
           Optional. Credentials to use with Storage Client.
 
     Returns:
-      A Dict object representing the JSON document.
+      A Dict object representing the YAML document.
     """
     if path.startswith("gs://"):
-        return _load_json_from_gs_uri(path, project, credentials)
+        return _load_yaml_from_gs_uri(path, project, credentials)
     else:
-        return _load_json_from_local_file(path)
+        return _load_yaml_from_local_file(path)
 
 
-def _load_json_from_gs_uri(
+def _load_yaml_from_gs_uri(
     uri: str,
     project: Optional[str] = None,
     credentials: Optional[auth_credentials.Credentials] = None,
 ) -> Dict[str, Any]:
-    """Loads data from a JSON document referenced by a GCS URI.
+    """Loads data from a YAML document referenced by a GCS URI.
 
     Args:
       path (str):
-          Required. GCS URI for JSON document.
+          Required. GCS URI for YAML document.
       project (str):
           Optional. Project to initiate the Storage client with.
       credentials (auth_credentials.Credentials):
           Optional. Credentials to use with Storage Client.
 
     Returns:
-      A Dict object representing the JSON document.
+      A Dict object representing the YAML document.
     """
     storage_client = storage.Client(project=project, credentials=credentials)
     blob = storage.Blob.from_string(uri, storage_client)
-    return json.loads(blob.download_as_bytes())
+    return yaml.safe_load(blob.download_as_bytes())
 
 
-def _load_json_from_local_file(file_path: str) -> Dict[str, Any]:
-    """Loads data from a JSON local file.
+def _load_yaml_from_local_file(file_path: str) -> Dict[str, Any]:
+    """Loads data from a YAML local file.
 
     Args:
       file_path (str):
-          Required. The local file path of the JSON document.
+          Required. The local file path of the YAML document.
 
     Returns:
-      A Dict object representing the JSON document.
+      A Dict object representing the YAML document.
     """
     with open(file_path) as f:
-        return json.load(f)
+        return yaml.safe_load(f)
