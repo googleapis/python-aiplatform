3224ae3402e9493866dd4958d011a431968b9c2c
Morgan Du <morgandu@google.com>
2022-04-22 12:52:40 -0700

    feat: allow creating featurestore without online node (#1180)

--- tests/system/aiplatform/test_featurestore.py
+++ tests/system/aiplatform/test_featurestore.py
@@ -65,7 +65,9 @@ class TestFeaturestore(e2e_base.TestEndToEnd):
         featurestore_id = self._make_display_name(key=_TEST_FEATURESTORE_ID).replace(
             "-", "_"
         )[:60]
-        featurestore = aiplatform.Featurestore.create(featurestore_id=featurestore_id)
+        featurestore = aiplatform.Featurestore.create(
+            featurestore_id=featurestore_id, online_store_fixed_node_count=1
+        )
 
         shared_state["resources"] = [featurestore]
         shared_state["featurestore"] = featurestore
