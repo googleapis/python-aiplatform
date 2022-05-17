91e809523b9c32366ae6cce5087dde58ba4274e6
Chun-Hsiang Wang <chunhsiang@google.com>
2022-05-17 02:15:34 +0000

    chore: Updated cpr system test.

--- tests/system/aiplatform/test_prediction_cpr.py
+++ tests/system/aiplatform/test_prediction_cpr.py
@@ -66,17 +66,19 @@ class TestPredictionCpr(e2e_base.TestEndToEnd):
             )
         assert len(json.loads(local_predict_response.content)["predictions"]) == 1
 
-        lasting_local_endpoint = local_model.deploy_to_local_endpoint_lastingly(
+        interactive_local_endpoint = local_model.deploy_to_local_endpoint(
             artifact_uri=_ARTIFACT_URI,
             credential_path=os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"),
         )
-        lasting_local_predict_response = lasting_local_endpoint.predict(
+        interactive_local_endpoint.serve()
+        interactive_local_predict_response = interactive_local_endpoint.predict(
             request=f'{{"instances": {_PREDICTION_INPUT}}}',
             headers={"Content-Type": "application/json"},
         )
-        lasting_local_endpoint.undeploy()
+        interactive_local_endpoint.stop()
         assert (
-            len(json.loads(lasting_local_predict_response.content)["predictions"]) == 1
+            len(json.loads(interactive_local_predict_response.content)["predictions"])
+            == 1
         )
 
         # Configure docker.
