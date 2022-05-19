54dcc13e07b610e5e7fdec9d807fcefc9a9f3cd4
Sam Goodman <goodmansam@google.com>
2022-05-19 00:28:37 +0000

    Nox fixes

--- tests/system/aiplatform/test_model_version_management.py
+++ tests/system/aiplatform/test_model_version_management.py
@@ -27,6 +27,7 @@ from google.cloud.aiplatform.models import ModelRegistry
 from tests.system.aiplatform import e2e_base
 from tests.system.aiplatform import test_model_upload
 
+
 @pytest.mark.usefixtures("delete_staging_bucket")
 class TestVersionManagement(e2e_base.TestEndToEnd):
 
@@ -47,15 +48,15 @@ class TestVersionManagement(e2e_base.TestEndToEnd):
         model_path = tempfile.mktemp() + ".my_model.xgb"
         model_blob.download_to_filename(filename=model_path)
 
-        model_id = 'my_model_id' + uuid.uuid4().hex
+        model_id = "my_model_id" + uuid.uuid4().hex
         version_description = "My description"
-        version_aliases = ['system-test-model', 'testing']
+        version_aliases = ["system-test-model", "testing"]
 
         model = aiplatform.Model.upload_xgboost_model_file(
             model_file_path=model_path,
             version_aliases=version_aliases,
             model_id=model_id,
-            version_description=version_description
+            version_description=version_description,
         )
         shared_state["resources"] = [model]
 
@@ -69,19 +70,17 @@ class TestVersionManagement(e2e_base.TestEndToEnd):
 
         assert model.version_description == version_description
         assert model.version_aliases == version_aliases
-        assert 'default' in model.version_aliases
+        assert "default" in model.version_aliases
 
         model2 = aiplatform.Model.upload_xgboost_model_file(
-            model_file_path=model_path,
-            parent_model=model_id,
-            is_default_version=False
+            model_file_path=model_path, parent_model=model_id, is_default_version=False
         )
         shared_state["resources"].append(model2)
 
-        assert model2.version_id == '2'
+        assert model2.version_id == "2"
         assert model2.resource_name == model.resource_name
         assert model2.version_aliases == []
-        '''
+        """
         # Test predictions use right version
         endpoint = model2.deploy(machine_type="n1-standard-2")
         shared_state["resources"].append(endpoint)
@@ -89,20 +88,36 @@ class TestVersionManagement(e2e_base.TestEndToEnd):
 
         assert len(predict_response.predictions) == 1
         assert predict_response.model_version_id == '2'
-        '''
+        """
 
         # Test that VersionInfo properties are correct.
         # Currently, get_version_info and list_versions don't yield identical
-        # resource names at this time due to a Model Registry bug (b/233118690) 
-        model_info = model2.versioning_registry.get_version_info('testing')
+        # resource names at this time due to a Model Registry bug (b/233118690)
+        model_info = model2.versioning_registry.get_version_info("testing")
         version_list = model2.versioning_registry.list_versions()
         assert len(version_list) == 2
         list_info = version_list[0]
         assert model_info.version_id == list_info.version_id == model.version_id
-        assert model_info.version_aliases == list_info.version_aliases == model.version_aliases
-        assert model_info.version_description == list_info.version_description == model.version_description
-        assert model_info.model_display_name == list_info.model_display_name == model.display_name
-        assert model_info.version_update_time == list_info.version_update_time == model.version_update_time
+        assert (
+            model_info.version_aliases
+            == list_info.version_aliases
+            == model.version_aliases
+        )
+        assert (
+            model_info.version_description
+            == list_info.version_description
+            == model.version_description
+        )
+        assert (
+            model_info.model_display_name
+            == list_info.model_display_name
+            == model.display_name
+        )
+        assert (
+            model_info.version_update_time
+            == list_info.version_update_time
+            == model.version_update_time
+        )
 
         # Test that get_model yields a new instance of `model`
         model_clone = model2.versioning_registry.get_model()
@@ -112,12 +127,12 @@ class TestVersionManagement(e2e_base.TestEndToEnd):
 
         # Test add and removal of aliases
         registry = ModelRegistry(model)
-        registry.add_version_aliases(['new-alias'], 'default')
-        registry.remove_version_aliases(['testing'], 'new-alias')
-        model = registry.get_model('new-alias')
-        assert 'testing' not in model.version_aliases
+        registry.add_version_aliases(["new-alias"], "default")
+        registry.remove_version_aliases(["testing"], "new-alias")
+        model = registry.get_model("new-alias")
+        assert "testing" not in model.version_aliases
 
         # Test deletion of a model version
-        registry.delete_version('2')
+        registry.delete_version("2")
         versions = registry.list_versions()
-        assert '2' not in [version.version_id for version in versions]
+        assert "2" not in [version.version_id for version in versions]
