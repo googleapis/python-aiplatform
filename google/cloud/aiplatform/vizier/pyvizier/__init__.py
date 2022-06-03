46cbe4aa00b6f0cc655d9dfe13e5ca0a28ecf54a
halio-g <halio@google.com>
2022-06-03 13:27:33 -0700

    imported the objects from the pyvizier.

--- google/cloud/aiplatform/vizier/pyvizier/__init__.py
+++ google/cloud/aiplatform/vizier/pyvizier/__init__.py
@@ -10,6 +10,11 @@ from google.cloud.aiplatform.vizier.pyvizier.base_study_config import SearchSpac
 from google.cloud.aiplatform.vizier.pyvizier.common import Metadata
 from google.cloud.aiplatform.vizier.pyvizier.common import MetadataValue
 from google.cloud.aiplatform.vizier.pyvizier.common import Namespace
+from google.cloud.aiplatform.vizier.pyvizier.proto_converters import ParameterConfigConverter
+from google.cloud.aiplatform.vizier.pyvizier.proto_converters import MeasurementConverter
+from google.cloud.aiplatform.vizier.pyvizier.study_config import StudyConfig
+from google.cloud.aiplatform.vizier.pyvizier.study_config import Algorithm
+from google.cloud.aiplatform.vizier.pyvizier.automated_stopping import AutomatedStoppingConfig
 from google.cloud.aiplatform.vizier.pyvizier.parameter_config import ExternalType
 from google.cloud.aiplatform.vizier.pyvizier.parameter_config import ParameterConfig
 from google.cloud.aiplatform.vizier.pyvizier.parameter_config import ParameterType
@@ -24,4 +29,3 @@ from google.cloud.aiplatform.vizier.pyvizier.trial import TrialFilter
 from google.cloud.aiplatform.vizier.pyvizier.trial import TrialStatus
 from google.cloud.aiplatform.vizier.pyvizier.trial import TrialSuggestion
 
-StudyConfig = ProblemStatement  # To be deprecated.
