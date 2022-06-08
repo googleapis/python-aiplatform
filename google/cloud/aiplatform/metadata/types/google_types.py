f74137187797ffbbb08b7b2aaeded5f62244532b
sina chavoshi <sina.chavoshi@gmail.com>
2022-06-08 02:17:01 +0000

    Add class for UnmanagedContainerModel

--- google/cloud/aiplatform/metadata/types/google_types.py
+++ google/cloud/aiplatform/metadata/types/google_types.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from typing import Optional, Dict , NamedTuple
+from typing import Optional, Dict, NamedTuple, List
 from google.cloud.aiplatform.metadata import artifact
 
 
@@ -102,6 +102,7 @@ class VertexTensorboardRun(artifact.BaseArtifactType):
             metadata=extended_metadata,
         )
 
+
 class VertexModel(artifact.BaseArtifactType):
     """An artifact representing a Vertex Model."""
 
@@ -143,6 +144,7 @@ class VertexModel(artifact.BaseArtifactType):
             description=description,
             metadata=extended_metadata,
         )
+
 
 class VertexEndpoint(artifact.BaseArtifactType):
     """An artifact representing a Vertex Endpoint."""
@@ -231,6 +233,7 @@ class BQMLModel(artifact.BaseArtifactType):
             metadata=extended_metadata,
         )
 
+
 class BQTable(artifact.BaseArtifactType):
     """An artifact representing a BQML Table."""
 
@@ -279,26 +282,48 @@ class BQTable(artifact.BaseArtifactType):
             metadata=extended_metadata,
         )
 
+
 class UnmanagedContainerModel(artifact.BaseArtifactType):
     """An artifact representing a Vertex Unmanaged Container Model."""
 
     def __init__(
         self,
-        bqml_project_id: Optional[str] = None,
-        bqml_dataset_id: Optional[str] = None,
-        bqml_model_id: Optional[str] = None,
+        instance_schema_uri: str,
+        parameters_schema_uri: str,
+        prediction_schema_uri: str,
+        image_uri: str,
+        command: Optional[List[str]] = None,
+        args: Optional[List[str]] = None,
+        env: Optional[List[Dict[str, str]]] = None,
+        ports: Optional[List[int]] = None,
+        predict_route: Optional[str] = None,
+        health_route: Optional[str] = None,
         display_name: Optional[str] = None,
         schema_version: Optional[str] = None,
         description: Optional[str] = None,
         metadata: Optional[Dict] = None,
     ):
         """Args:
-        bqml_project_id (str):
-            The Project that hosts the corresponding BigQuery ML Model.
-        bqml_dataset_id (str):
-            The BigQuery Dataset ID for corresponding BigQuery ML Model.
-        bqml_model_id (str):
-            The BigQuery Model ID for the corresponding Model.
+        instance_schema_uri (str):
+            Points to a YAML file stored on Google Cloud Storage describing the format of a single instance, which are used in PredictRequest.instances, ExplainRequest.instances and BatchPredictionJob.input_config. The schema is defined as an OpenAPI 3.0.2 `Schema Object.
+        parameters_schema_uri (str):
+            Points to a YAML file stored on Google Cloud Storage describing the parameters of prediction and explanation via PredictRequest.parameters, ExplainRequest.parameters and BatchPredictionJob.model_parameters. The schema is defined as an OpenAPI 3.0.2 `Schema Object.
+        prediction_schema_uri (str):
+            Points to a YAML file stored on Google Cloud Storage describing the format of a single prediction produced by this Model, which are returned via PredictResponse.predictions, ExplainResponse.explanations, and BatchPredictionJob.output_config. The schema is defined as an OpenAPI 3.0.2 `Schema Object.
+        image_uri (str):
+            Required. URI of the Docker image to be used as the custom container for serving predictions. This URI must identify an image in Artifact Registry or Container Registry. Learn more about the `container publishing requirements
+        command (Sequence[str]):
+            Specifies the command that runs when the container starts. This overrides the container's `ENTRYPOINT
+        args (Sequence[str]):
+            Specifies arguments for the command that runs when the container starts. This overrides the container's ```CMD``
+        env (Sequence[google.cloud.aiplatform_v1.types.EnvVar]):
+            List of environment variables to set in the container. After the container starts running, code running in the container can read these environment variables. Additionally, the command and args fields can reference these variables. Later entries in this list can also reference earlier entries. For example, the following example sets the variable ``VAR_2`` to have the value ``foo bar``: .. code:: json [ { "name": "VAR_1", "value": "foo" }, { "name": "VAR_2", "value": "$(VAR_1) bar" } ] If you switch the order of the variables in the example, then the expansion does not occur. This field corresponds to the ``env`` field of the Kubernetes Containers `v1 core API.
+        ports (Sequence[google.cloud.aiplatform_v1.types.Port]):
+            List of ports to expose from the container. Vertex AI sends any prediction requests that it receives to the first port on this list. Vertex AI also sends `liveness and health checks.
+        predict_route (str):
+            HTTP path on the container to send prediction requests to. Vertex AI forwards requests sent using projects.locations.endpoints.predict to this path on the container's IP address and port. Vertex AI then returns the container's response in the API response. For example, if you set this field to ``/foo``, then when Vertex AI receives a prediction request, it forwards the request body in a POST request to the ``/foo`` path on the port of your container specified by the first value of this ``ModelContainerSpec``'s ports field. If you don't specify this field, it defaults to the following value when you [deploy this Model to an Endpoint][google.cloud.aiplatform.v1.EndpointService.DeployModel]: /v1/endpoints/ENDPOINT/deployedModels/DEPLOYED_MODEL:predict The placeholders in this value are replaced as follows: - ENDPOINT: The last segment (following ``endpoints/``)of the Endpoint.name][] field of the Endpoint where this Model has been deployed. (Vertex AI makes this value available to your container code as the ```AIP_ENDPOINT_ID`` environment variable
+        health_route (str):
+            HTTP path on the container to send health checks to. Vertex AI intermittently sends GET requests to this path on the container's IP address and port to check that the container is healthy. Read more about `health checks
         display_name (str):
             Optional. The user-defined name of the Artifact.
         schema_version (str):
@@ -310,10 +335,31 @@ class UnmanagedContainerModel(artifact.BaseArtifactType):
             Optional. Contains the metadata information that will be stored in the Artifact.
         """
         SCHEMA_TITLE = "google.UnmanagedContainerModel"
+
         extended_metadata = metadata or {}
-        extended_metadata["projectId"] = bqml_project_id
-        extended_metadata["datasetId"] = bqml_dataset_id
-        extended_metadata["modelId"] = bqml_model_id
+        extended_metadata["predictSchemata"] = {}
+        extended_metadata["predictSchemata"]["instanceSchemaUri"] = instance_schema_uri
+        extended_metadata["predictSchemata"][
+            "parametersSchemaUri"
+        ] = parameters_schema_uri
+        extended_metadata["predictSchemata"][
+            "predictionSchemaUri"
+        ] = prediction_schema_uri
+
+        extended_metadata["containerSpec"] = {}
+        extended_metadata["containerSpec"]["imageUri"] = image_uri
+        if command:
+            extended_metadata["containerSpec"]["command"] = command
+        if args:
+            extended_metadata["containerSpec"]["args"] = args
+        if env:
+            extended_metadata["containerSpec"]["env"] = env
+        if ports:
+            extended_metadata["containerSpec"]["ports"] = ports
+        if predict_route:
+            extended_metadata["containerSpec"]["predictRoute"] = predict_route
+        if health_route:
+            extended_metadata["containerSpec"]["healthRoute"] = health_route
 
         super(UnmanagedContainerModel, self).__init__(
             schema_title=SCHEMA_TITLE,
