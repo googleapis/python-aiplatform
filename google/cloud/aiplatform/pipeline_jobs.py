480359aa5e4003cd7e19f1eb35bd476294d098fa
Sara Robinson <sararob@google.com>
2022-07-06 19:46:13 -0400

    updates from reviewer feedback

--- google/cloud/aiplatform/pipeline_jobs.py
+++ google/cloud/aiplatform/pipeline_jobs.py
@@ -22,6 +22,7 @@ import re
 from typing import Any, Dict, List, Optional, Union
 
 from google.auth import credentials as auth_credentials
+from google.cloud import aiplatform
 from google.cloud.aiplatform import base
 from google.cloud.aiplatform import initializer
 from google.cloud.aiplatform import utils
@@ -777,30 +778,37 @@ class PipelineJob(
 
         return cloned
 
-    def get_associated_experiment(self) -> Optional["experiment_resources.Experiment"]:
-        """Returns the aiplatform.Experiment associated with this PipelineJob,
-        or None if this PipelineJob is not associated with an experiment."""
+    def get_associated_experiment(self) -> Optional["aiplatform.Experiment"]:
+        """Gets the aiplatform.Experiment associated with this PipelineJob,
+        or None if this PipelineJob is not associated with an experiment.
+
+        Returns:
+            An aiplatform.Experiment resource or None if this PipelineJob is
+            not associated with an experiment..
+
+        """
 
         pipeline_parent_contexts = (
             self._gca_resource.job_detail.pipeline_run_context.parent_contexts
         )
-        pipeline_context_resources = [
-            context._Context(resource_name=i)._gca_resource
-            for i in pipeline_parent_contexts
+
+        pipeline_experiment_resource_names = [
+            c
+            for c in pipeline_parent_contexts
+            if c != self._gca_resource.job_detail.pipeline_context.name
         ]
-
-        pipeline_experiment_resource_names = []
-
-        for i in pipeline_context_resources:
-            if i.schema_title == "system.Experiment":
-                pipeline_experiment_resource_names.append(i.name.split("contexts/")[1])
 
         if len(pipeline_experiment_resource_names) > 1:
             _LOGGER.warning(
-                "There are more than experiments associated with this pipeline, returning only one..."
+                f"There is more than one Experiment is associated with this pipeline."
+                f"The following experiments were found: {pipeline_experiment_resource_names.join(', ')}\n"
+                f"Returning only the following experiment: {pipeline_experiment_resource_names[0]}"
             )
 
         if len(pipeline_experiment_resource_names) >= 1:
             return experiment_resources.Experiment(
-                pipeline_experiment_resource_names[0]
+                pipeline_experiment_resource_names[0],
+                project=self.project,
+                location=self.location,
+                credentials=self.credentials,
             )
