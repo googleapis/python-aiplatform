e35682c22174ff6996d29f111fee5dc487234ec5
Anthonios Partheniou <partheniou@google.com>
2022-07-12 09:32:36 -0400

    update replacement in owlbot.py

--- owlbot.py
+++ owlbot.py
@@ -5,7 +5,7 @@
 # You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
-#
+#https://github.com/googleapis/python-aiplatform/pull/14743.6
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -130,7 +130,7 @@ s.replace(
 
 s.replace(
     ".kokoro/test-samples-impl.sh",
-    "python3.6",
+    "python3.9",
     "python3",
 )
 
