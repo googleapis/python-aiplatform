1dcae4bd30e8eff5c33c3a1e2abbbeba83985f65
Siping Hu <sipingh@google.com>
2022-07-25 12:48:22 -0700

    Modify unit tests to test_models.py

--- tests/unit/aiplatform/test_models.py
+++ tests/unit/aiplatform/test_models.py
@@ -946,47 +946,6 @@ class TestModel:
         )
 
     @pytest.mark.parametrize("sync", [True, False])
-    def test_upload_uploads_and_gets_model(
-        self, upload_model_mock, get_model_mock, sync
-    ):
-
-        my_model = models.Model.upload(
-            display_name=_TEST_MODEL_NAME,
-            serving_container_image_uri=_TEST_SERVING_CONTAINER_IMAGE,
-            serving_container_predict_route=_TEST_SERVING_CONTAINER_PREDICTION_ROUTE,
-            serving_container_health_route=_TEST_SERVING_CONTAINER_HEALTH_ROUTE,
-            sync=sync,
-            upload_request_timeout=None,
-        )
-
-        if not sync:
-            my_model.wait()
-
-        container_spec = gca_model.ModelContainerSpec(
-            image_uri=_TEST_SERVING_CONTAINER_IMAGE,
-            predict_route=_TEST_SERVING_CONTAINER_PREDICTION_ROUTE,
-            health_route=_TEST_SERVING_CONTAINER_HEALTH_ROUTE,
-        )
-
-        managed_model = gca_model.Model(
-            display_name=_TEST_MODEL_NAME,
-            container_spec=container_spec,
-            version_aliases=["default"],
-        )
-
-        upload_model_mock.assert_called_once_with(
-            request=gca_model_service.UploadModelRequest(
-                parent=initializer.global_config.common_location_path(),
-                model=managed_model,
-            ),
-            timeout=None,
-        )
-
-        get_model_mock.assert_called_once_with(
-            name=_TEST_MODEL_RESOURCE_NAME, retry=base._DEFAULT_RETRY
-        )
-
-    @pytest.mark.parametrize("sync", [True, False])
     def test_upload_uploads_and_gets_model_with_all_args(
         self, upload_model_mock, get_model_mock, sync
     ):
