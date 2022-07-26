c5811a3779b0697232de3cf2446473352eed784c
Alexey Volkov <alexey.volkov@ark-kun.com>
2022-07-25 18:42:53 -0700

    fix: Fixed getting the output GCS bucket in PipelineJob.submit

--- google/cloud/aiplatform/pipeline_jobs.py
+++ google/cloud/aiplatform/pipeline_jobs.py
@@ -344,7 +344,7 @@ class PipelineJob(
             self._gca_resource.network = network
 
         gcs_utils.create_gcs_bucket_for_pipeline_artifacts_if_it_does_not_exist(
-            output_artifacts_gcs_dir=self.pipeline_spec.get("gcsOutputDirectory"),
+            output_artifacts_gcs_dir=self._gca_resource.runtime_config.gcs_output_directory,
             service_account=self._gca_resource.service_account,
             project=self.project,
             location=self.location,
