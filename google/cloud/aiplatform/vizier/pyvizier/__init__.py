b2b2428e6b8268f2b741d8a2cb7d65e9511661bd
halio-g <halio@google.com>
2022-07-28 10:08:17 -0700

    Add the wrapper to give more error information about the vizier import error.

--- google/cloud/aiplatform/vizier/pyvizier/__init__.py
+++ google/cloud/aiplatform/vizier/pyvizier/__init__.py
@@ -1,34 +1,40 @@
 """PyVizier classes for Pythia policies."""
 
-from vizier.pyvizier import MetricInformation
-from vizier.pyvizier import MetricsConfig
-from vizier.pyvizier import MetricType
-from vizier.pyvizier import (
-    ObjectiveMetricGoal,
-)
-from vizier.pyvizier import ProblemStatement
-from vizier.pyvizier import SearchSpace
-from vizier.pyvizier import (
-    SearchSpaceSelector,
-)
-from vizier.pyvizier import Metadata
-from vizier.pyvizier import MetadataValue
-from vizier.pyvizier import Namespace
-from vizier.pyvizier import ExternalType
-from vizier.pyvizier import ParameterConfig
-from vizier.pyvizier import ParameterType
-from vizier.pyvizier import ScaleType
-from vizier.pyvizier import CompletedTrial
-from vizier.pyvizier import Measurement
-from vizier.pyvizier import MonotypeParameterSequence
-from vizier.pyvizier import Metric
-from vizier.pyvizier import ParameterDict
-from vizier.pyvizier import ParameterValue
-from vizier.pyvizier import Trial
-from vizier.pyvizier import ParameterValueTypes
-from vizier.pyvizier import TrialFilter
-from vizier.pyvizier import TrialStatus
-from vizier.pyvizier import TrialSuggestion
+try:
+    from vizier.pyvizier import MetricInformation
+    from vizier.pyvizier import MetricsConfig
+    from vizier.pyvizier import MetricType
+    from vizier.pyvizier import (
+        ObjectiveMetricGoal,
+    )
+    from vizier.pyvizier import ProblemStatement
+    from vizier.pyvizier import SearchSpace
+    from vizier.pyvizier import (
+        SearchSpaceSelector,
+    )
+    from vizier.pyvizier import Metadata
+    from vizier.pyvizier import MetadataValue
+    from vizier.pyvizier import Namespace
+    from vizier.pyvizier import ExternalType
+    from vizier.pyvizier import ParameterConfig
+    from vizier.pyvizier import ParameterType
+    from vizier.pyvizier import ScaleType
+    from vizier.pyvizier import CompletedTrial
+    from vizier.pyvizier import Measurement
+    from vizier.pyvizier import MonotypeParameterSequence
+    from vizier.pyvizier import Metric
+    from vizier.pyvizier import ParameterDict
+    from vizier.pyvizier import ParameterValue
+    from vizier.pyvizier import Trial
+    from vizier.pyvizier import ParameterValueTypes
+    from vizier.pyvizier import TrialFilter
+    from vizier.pyvizier import TrialStatus
+    from vizier.pyvizier import TrialSuggestion
+except ImportError:
+    raise ImportError(
+        "Google-vizier is not installed, and is required to use Vizier client."
+        'Please install the SDK using "pip install google-vizier==0.0.3a"'
+    )
 
 from google.cloud.aiplatform.vizier.pyvizier.proto_converters import TrialConverter
 from google.cloud.aiplatform.vizier.pyvizier.proto_converters import (
