783ad5d2bc991df85878d81ba84592ec04924288
Chun-Hsiang Wang <chunhsiang@google.com>
2022-08-01 17:24:46 +0000

    fix: Search workdir then users' src dir name for CPR model servers.

--- google/cloud/aiplatform/prediction/model_server.py
+++ google/cloud/aiplatform/prediction/model_server.py
@@ -189,7 +189,7 @@ def set_number_of_workers_from_env() -> None:
 if __name__ == "__main__":
     cpr_user_dir_name = os.getenv("CPR_USER_DIR_NAME")
     if cpr_user_dir_name:
-        sys.path.insert(0, os.path.join(os.getcwd(), cpr_user_dir_name))
+        sys.path.insert(1, os.path.join(os.getcwd(), cpr_user_dir_name))
 
     set_number_of_workers_from_env()
     uvicorn.run(
