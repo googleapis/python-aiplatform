a190837f960a7fc10b58ad3c58e5f9f17388b6e1
siping-hu <55422045+siping-hu@users.noreply.github.com>
2022-08-31 09:06:04 -0700

    Update models.py
    Linting fix. Use implicit True/False evaluations

--- google/cloud/aiplatform/models.py
+++ google/cloud/aiplatform/models.py
@@ -2910,7 +2910,7 @@ class Model(base.VertexAiResourceNounWithFutureManager):
         if labels:
             utils.validate_labels(labels)
 
-        if bool(explanation_metadata) == True and bool(explanation_parameters) == False:
+        if bool(explanation_metadata) and not bool(explanation_parameters):
             raise ValueError(
                 "To get model explanation, `explanation_parameters` must be specified."
             )
