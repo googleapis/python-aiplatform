3e95e8d85752332d8147c1159b03db73257f2c51
A Vertex SDK engineer <vertex-sdk-bot@google.com>
2022-11-18 18:08:00 -0800

    chore: Add OP 2.10 images to first-party support.
    PiperOrigin-RevId: 489600866

--- google/cloud/aiplatform/constants/prediction.py
+++ google/cloud/aiplatform/constants/prediction.py
@@ -79,6 +79,12 @@ SKLEARN_CONTAINER_URIS = [
 ]
 
 TF_CONTAINER_URIS = [
+    "us-docker.pkg.dev/vertex-ai/prediction/tf2-cpu.2-10:latest",
+    "europe-docker.pkg.dev/vertex-ai/prediction/tf2-cpu.2-10:latest",
+    "asia-docker.pkg.dev/vertex-ai/prediction/tf2-cpu.2-10:latest",
+    "us-docker.pkg.dev/vertex-ai/prediction/tf2-gpu.2-10:latest",
+    "europe-docker.pkg.dev/vertex-ai/prediction/tf2-gpu.2-10:latest",
+    "asia-docker.pkg.dev/vertex-ai/prediction/tf2-gpu.2-10:latest",
     "us-docker.pkg.dev/vertex-ai/prediction/tf2-cpu.2-9:latest",
     "europe-docker.pkg.dev/vertex-ai/prediction/tf2-cpu.2-9:latest",
     "asia-docker.pkg.dev/vertex-ai/prediction/tf2-cpu.2-9:latest",
