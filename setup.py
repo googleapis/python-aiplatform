5b0a5537bbec3a16e05ec3f880e30e341c54bb1b
nayaknishant <nishantnayak@google.com>
2023-03-20 11:22:45 -0700

    feat: adding Python 3.11 support

--- setup.py
+++ setup.py
@@ -171,7 +171,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
