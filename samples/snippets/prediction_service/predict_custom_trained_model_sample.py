bad0ee2d5b59d424e3a68e60a126afd0639270b6
Rocha <rocha_mrsm@hotmail.com>
2023-03-23 20:43:47 -0300

    Update predict_custom_trained_model_sample.py
    This snippet of code were giving me this error:
    
    google.api_core.exceptions.FailedPrecondition: 400 "Prediction failed: Exception during sklearn prediction: predict() got an unexpected keyword argument 'parameters'"
    
    Deleting the step of parameters, I was able to use the endpoint as I'm predicting a local model, for example. I'm also suggesting return the predictions instead of a print to let the user decide what to do with the response.
    
    With these changes I'm able to perform this following code:
    
    pred = predict_custom_trained_model_sample(
        project=project,
        endpoint_id=endrpoind_id,
        location=location,
        instances=[[5.1, 3.5, 1.4, 0.2], [5.1, 3.5, 1.4, 0.2]]
    )

--- samples/snippets/prediction_service/predict_custom_trained_model_sample.py
+++ samples/snippets/prediction_service/predict_custom_trained_model_sample.py
@@ -41,20 +41,18 @@ def predict_custom_trained_model_sample(
     instances = [
         json_format.ParseDict(instance_dict, Value()) for instance_dict in instances
     ]
-    parameters_dict = {}
-    parameters = json_format.ParseDict(parameters_dict, Value())
+
     endpoint = client.endpoint_path(
         project=project, location=location, endpoint=endpoint_id
     )
     response = client.predict(
-        endpoint=endpoint, instances=instances, parameters=parameters
+        endpoint=endpoint, instances=instances
     )
     print("response")
     print(" deployed_model_id:", response.deployed_model_id)
     # The predictions are a google.protobuf.Value representation of the model's predictions.
     predictions = response.predictions
-    for prediction in predictions:
-        print(" prediction:", dict(prediction))
+    return predictions
 
 
 # [END aiplatform_predict_custom_trained_model_sample]
