895ff8d324bb951706bbcd94b1414f5b5e181265
Anthonios Partheniou <partheniou@google.com>
2023-06-27 11:25:46 -0400

    chore: use the latest python post processor image

--- owlbot.py
+++ owlbot.py
@@ -85,6 +85,9 @@ for library in s.get_staging_dirs(default_version):
 
 s.remove_staging_dirs()
 
+# Remove this line once the owlbot post processor runs
+has_generator_updates = True
+
 # only run post processor when there are changes to the generated code
 if has_generator_updates:
 
