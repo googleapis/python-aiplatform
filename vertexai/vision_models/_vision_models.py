ecfb84ee66453f92c9350449a2d7ee0736f12d50
Dharmick Sai Kantamneni <dharmicksai@google.com>
2023-10-14 15:41:56 +0530

    fix: Include negative prompt in parameters for Image Generation
    Added negativePromt as part of parameters during image generation.

--- vertexai/vision_models/_vision_models.py
+++ vertexai/vision_models/_vision_models.py
@@ -207,6 +207,8 @@ class ImageGenerationModel(
                 parameters["aspectRatio"] = f"{width}:{height}"
 
         parameters["sampleCount"] = number_of_images
+        if negative_prompt:
+            parameters["negativePrompt"] = negative_prompt
 
         if seed is not None:
             # Note: String seed and numerical seed give different results
