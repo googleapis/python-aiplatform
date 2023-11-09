24739f958b48b872cb25b18668a71fb480f5256f
Matthew Tang <tangmatthew@google.com>
2023-11-09 11:38:12 -0800

    chore: Expand Ray on Vertex cluster management system test
    PiperOrigin-RevId: 580975941

--- tests/system/vertex_ray/test_cluster_management.py
+++ tests/system/vertex_ray/test_cluster_management.py
@@ -20,13 +20,36 @@ from google.cloud.aiplatform.preview import vertex_ray
 from tests.system.aiplatform import e2e_base
 import ray
 
+RAY_VERSION = "2.4.0"
+CLUSTER_RAY_VERSION = "2_4"
+PROJECT_ID = "ucaip-sample-tests"
+
 
 class TestClusterManagement(e2e_base.TestEndToEnd):
     _temp_prefix = "temp-rov-cluster-management"
 
     def test_cluster_management(self):
-        assert ray.__version__ == "2.4.0"
-        aiplatform.init(project="ucaip-sample-tests", location="us-central1")
+        assert ray.__version__ == RAY_VERSION
+        aiplatform.init(project=PROJECT_ID, location="us-central1")
+
+        # CPU default cluster
+        head_node_type = vertex_ray.Resources()
+        worker_node_types = [vertex_ray.Resources()]
+
+        cluster_resource_name = vertex_ray.create_ray_cluster(
+            head_node_type=head_node_type,
+            worker_node_types=worker_node_types,
+        )
+
+        cluster_details = vertex_ray.get_ray_cluster(cluster_resource_name)
+        assert cluster_details.ray_version == CLUSTER_RAY_VERSION
+        assert cluster_details.state == "RUNNING"
 
         clusters = vertex_ray.list_ray_clusters()
-        assert clusters[0].ray_version == "2_4"
+        assert clusters[0].ray_version == CLUSTER_RAY_VERSION
+        assert clusters[0].state == "RUNNING"
+
+        vertex_ray.delete_ray_cluster(cluster_resource_name)
+        # Ensure cluster was deleted
+        for cluster in vertex_ray.list_ray_clusters():
+            assert cluster.cluster_resource_name != cluster_resource_name
