4e891f7f0c83dabde429ffaa38166af5a44eab5a
A Vertex SDK engineer <vertex-sdk-bot@google.com>
2023-12-14 16:20:01 -0800

    fix: Pin google-cloud-aiplatform[tensorboard] dependency on tensorflow < 2.15.0 due to breaking change introduced in tensorboard 2.15.1
    PiperOrigin-RevId: 591081669

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ packages += [
     if package.startswith("google.cloud.aiplatform.preview.vertex_ray")
 ]
 
-tensorboard_extra_require = ["tensorflow >=2.3.0, <3.0.0dev"]
+tensorboard_extra_require = ["tensorflow >=2.3.0, <2.15.0"]
 metadata_extra_require = ["pandas >= 1.0.0", "numpy>=1.15.0"]
 xai_extra_require = ["tensorflow >=2.3.0, <3.0.0dev"]
 lit_extra_require = [
