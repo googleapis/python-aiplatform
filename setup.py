80d5c564a0a7f4b7acd1f4dadf73e52f4ac19a74
Matthew Tang <tangmatthew@google.com>
2024-01-17 15:05:05 -0800

    chore: Bump kfp version and update testing requirements to be compatible with protobuf4
    PiperOrigin-RevId: 599301198

--- setup.py
+++ setup.py
@@ -74,10 +74,7 @@ datasets_extra_require = [
 ]
 
 vizier_extra_require = [
-    "google-vizier==0.0.4; python_version=='3.8'",
-    "google-vizier==0.0.11; python_version=='3.9'",
-    "google-vizier>=0.0.14; python_version=='3.10'",
-    "google-vizier>=0.1.6; python_version>='3.11'",
+    "google-vizier>=0.1.6",
 ]
 
 prediction_extra_require = [
@@ -138,7 +135,7 @@ testing_extra_require = (
         "bigframes; python_version>='3.10'",
         "grpcio-testing",
         "ipython",
-        "kfp",
+        "kfp >= 2.6.0, < 3.0.0",
         "pyfakefs",
         "pytest-asyncio",
         "pytest-xdist",
