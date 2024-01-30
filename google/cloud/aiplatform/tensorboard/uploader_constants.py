4cab4d99e04494c1ad9d29e87e94933f7ad75d61
A Vertex SDK engineer <vertex-sdk-bot@google.com>
2024-01-30 10:12:55 -0800

    chore: Move Tensorboard uploader default values to constants file.
    PiperOrigin-RevId: 602764276

--- google/cloud/aiplatform/tensorboard/uploader_constants.py
+++ google/cloud/aiplatform/tensorboard/uploader_constants.py
@@ -21,3 +21,38 @@ ALLOWED_PLUGINS = [
     images_metadata.PLUGIN_NAME,
     graphs_metadata.PLUGIN_NAME,
 ]
+
+# Minimum length of a logdir polling cycle in seconds. Shorter cycles will
+# sleep to avoid spinning over the logdir, which isn't great for disks and can
+# be expensive for network file systems.
+MIN_LOGDIR_POLL_INTERVAL_SECS = 1
+
+# Maximum length of a base-128 varint as used to encode a 64-bit value
+# (without the "msb of last byte is bit 63" optimization, to be
+# compatible with protobuf and golang varints).
+MAX_VARINT64_LENGTH_BYTES = 10
+
+# Default minimum interval between initiating WriteTensorbordRunData RPCs in
+# milliseconds.
+DEFAULT_MIN_SCALAR_REQUEST_INTERVAL = 10
+
+# Default maximum WriteTensorbordRunData request size in bytes.
+DEFAULT_MAX_SCALAR_REQUEST_SIZE = 128 * (2**10)  # 128KiB
+
+# Default minimum interval between initiating WriteTensorbordRunData RPCs in
+# milliseconds.
+DEFAULT_MIN_TENSOR_REQUEST_INTERVAL = 10
+
+# Default minimum interval between initiating WriteTensorbordRunData RPCs in
+# milliseconds.
+DEFAULT_MIN_BLOB_REQUEST_INTERVAL = 10
+
+# Default maximum WriteTensorbordRunData request size in bytes.
+DEFAULT_MAX_TENSOR_REQUEST_SIZE = 512 * (2**10)  # 512KiB
+
+DEFAULT_MAX_BLOB_REQUEST_SIZE = 128 * (2**10)  # 24KiB
+
+# Default maximum tensor point size in bytes.
+DEFAULT_MAX_TENSOR_POINT_SIZE = 16 * (2**10)  # 16KiB
+
+DEFAULT_MAX_BLOB_SIZE = 10 * (2**30)  # 10GiB
