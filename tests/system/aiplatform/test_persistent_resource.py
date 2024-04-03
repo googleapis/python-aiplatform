b8b42a3150bb10942420b9ae66e7b8ccbed0d0d1
Qiao Wang <qiaowang@google.com>
2024-04-03 12:33:02 -0700

    test: Fix failing system test of import error
    PiperOrigin-RevId: 621606247

--- tests/system/aiplatform/test_persistent_resource.py
+++ tests/system/aiplatform/test_persistent_resource.py
@@ -19,13 +19,13 @@ import importlib
 
 from google.cloud import aiplatform
 from google.cloud.aiplatform import initializer
+from google.cloud.aiplatform import persistent_resource
 from google.cloud.aiplatform.compat.types import (
     machine_resources_v1beta1 as gca_machine_resources,
 )
 from google.cloud.aiplatform.compat.types import (
     persistent_resource_v1beta1 as gca_persistent_resource,
 )
-from google.cloud.aiplatform.preview import persistent_resource
 from tests.system.aiplatform import e2e_base
 import pytest
 
