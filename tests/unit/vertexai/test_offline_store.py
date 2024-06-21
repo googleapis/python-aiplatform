3e3671fca1d456661019038caf9e74ad1d68c3d3
A Vertex SDK engineer <vertex-sdk-bot@google.com>
2024-06-20 20:44:49 -0700

    chore: add inital impl for fetch_historical_feature_values
    PiperOrigin-RevId: 645246122

--- tests/unit/vertexai/test_offline_store.py
+++ tests/unit/vertexai/test_offline_store.py
@@ -15,12 +15,394 @@
 # limitations under the License.
 #
 
+from typing import List, Optional
+from unittest import mock
+import pytest
+import sys
 from vertexai.resources.preview.feature_store import (
     offline_store,
+    FeatureGroup,
+    Feature,
 )
-import pytest
+
+try:
+    import pandas as pd
+except ImportError:
+    pd = None
 
 
-def test_fetch_historical_feature_values():
-    with pytest.raises(NotImplementedError):
-        offline_store.fetch_historical_feature_values()
+# Constants used in tests
+ENTITY_ID_COL = "customer_id"
+
+# Data used to construct customer entity DF
+ENTITY_DF_TS_COL = "timestamp"
+ENTITY_DF_COLUMNS = ["customer_id", "neg_id", ENTITY_DF_TS_COL]
+
+CUSTOMER_1_OLD_ENTITY_DF_TIMESTAMP = pd.Timestamp("2023-12-12T12:23")
+CUSTOMER_1_OLD_ENTITY_DF_ENTRY = ["customer1", -1, CUSTOMER_1_OLD_ENTITY_DF_TIMESTAMP]
+CUSTOMER_2_OLD_ENTITY_DF_TIMESTAMP = pd.Timestamp("2023-12-12T13:13")
+CUSTOMER_2_OLD_ENTITY_DF_ENTRY = ["customer2", -2, CUSTOMER_2_OLD_ENTITY_DF_TIMESTAMP]
+
+CUSTOMER_1_NEW_ENTITY_DF_TIMESTAMP = pd.Timestamp("2025-01-01T17:00")
+CUSTOMER_1_NEW_ENTITY_DF_ENTRY = ["customer1", -1, CUSTOMER_1_NEW_ENTITY_DF_TIMESTAMP]
+CUSTOMER_2_NEW_ENTITY_DF_TIMESTAMP = pd.Timestamp("2025-12-12T18:22")
+CUSTOMER_2_NEW_ENTITY_DF_ENTRY = ["customer2", -2, CUSTOMER_2_NEW_ENTITY_DF_TIMESTAMP]
+
+# Data used to construct expected output of fetch historical feature values
+OUTPUT_DF_COLUMNS = [
+    "customer_id",
+    "neg_id",
+    "customer_number",
+    "customer_value",
+    "customer_truth",
+    ENTITY_DF_TS_COL,
+]
+CUSTOMER_1_OLD_DATA_EXPECTED_OUTPUT = [
+    "customer1",
+    -1,
+    1,
+    2,
+    False,
+    CUSTOMER_1_OLD_ENTITY_DF_TIMESTAMP,
+]
+CUSTOMER_2_OLD_DATA_EXPECTED_OUTPUT = [
+    "customer2",
+    -2,
+    2,
+    4,
+    False,
+    CUSTOMER_2_OLD_ENTITY_DF_TIMESTAMP,
+]
+CUSTOMER_1_NEW_DATA_EXPECTED_OUTPUT = [
+    "customer1",
+    -1,
+    1,
+    1,
+    True,
+    CUSTOMER_1_NEW_ENTITY_DF_TIMESTAMP,
+]
+CUSTOMER_2_NEW_DATA_EXPECTED_OUTPUT = [
+    "customer2",
+    -2,
+    2,
+    2,
+    True,
+    CUSTOMER_2_NEW_ENTITY_DF_TIMESTAMP,
+]
+
+FAKE_ENTITY_DF_QUERY = """ENTITY DF QUERY Line 1
+ENTITY DF QUERY Line 2
+ENTITY DF QUERY Line 3"""
+
+
+class FakeBigframe:
+    @property
+    def columns(self):
+        pass
+
+    @property
+    def sql(self):
+        pass
+
+    def select_dtypes(self, *args):
+        pass
+
+
+# Mock bigframe import
+bpd_module = type(sys)("bigframes.pandas")
+bpd_module.DataFrame = FakeBigframe
+bpd_module.read_gbq_query = lambda x: x
+sys.modules["bigframes.pandas"] = bpd_module
+
+bigframe_module = type(sys)("bigframes")
+bigframe_module.pandas = bpd_module
+bigframe_module.enums = mock.Mock()
+sys.modules["bigframes"] = bigframe_module
+
+# And now import bigframes
+import bigframes  # noqa: E402
+import bigframes.pandas as bpd  # noqa: E402
+
+
+pytestmark = pytest.mark.usefixtures("google_auth_mock")
+
+
+@pytest.fixture()
+def mock_convert_to_bigquery_dataframe():
+    with mock.patch.object(
+        offline_store._DataFrameToBigQueryDataFramesConverter, "to_bigquery_dataframe"
+    ) as mock_convert_to_bigquery_dataframe:
+        yield mock_convert_to_bigquery_dataframe
+
+
+def mock_bdf(
+    non_ts_cols: List[str],
+    ts_cols: Optional[List[str]] = None,
+    sql: Optional[str] = None,
+):
+    class MockBdf(bpd.DataFrame):
+        def __init__(self):
+            pass
+
+        def __contains__(self, key):
+            pass
+
+    if not ts_cols:
+        ts_cols = []
+    all_columns = non_ts_cols + ts_cols
+
+    m = mock.MagicMock(spec_set=MockBdf)
+    m.__contains__ = lambda s, k: k in all_columns
+    m.columns = all_columns
+    m.sql = sql
+
+    # Mock returned dtypes (FS code only uses `select_dtypes`).
+    dtypes_mock = mock.MagicMock()
+    dtypes_mock.columns = ts_cols
+    m.select_dtypes.return_value = dtypes_mock
+
+    return m
+
+
+@pytest.mark.parametrize(
+    "features",
+    [
+        None,
+        [],
+    ],
+)
+def test_no_features_raises_error(features):
+    with pytest.raises(ValueError) as excinfo:
+        offline_store.fetch_historical_feature_values(
+            entity_df=pd.DataFrame(),
+            features=features,
+        )
+    assert str(excinfo.value) == "Please specify a non-empty list of features."
+
+
+def test_entity_df_no_timestamp_column_raises_error(mock_convert_to_bigquery_dataframe):
+    with pytest.raises(ValueError) as excinfo:
+        mock_convert_to_bigquery_dataframe.return_value = mock_bdf(
+            non_ts_cols=["hi"], sql=None
+        )
+
+        offline_store.fetch_historical_feature_values(
+            entity_df=pd.DataFrame(),
+            features=["fake.feature"],
+        )
+
+    assert 'No timestamp column ("datetime" dtype) found in entity DataFrame.' == str(
+        excinfo.value
+    )
+
+
+def test_entity_df_too_many_timestamp_columns_raises_error(
+    mock_convert_to_bigquery_dataframe,
+):
+    with pytest.raises(ValueError) as excinfo:
+        bdf = mock_bdf(non_ts_cols=["hi"], ts_cols=["ts1", "ts2"], sql=None)
+        mock_convert_to_bigquery_dataframe.return_value = bdf
+
+        offline_store.fetch_historical_feature_values(
+            entity_df=pd.DataFrame(),
+            features=["fake.feature"],
+        )
+
+    assert (
+        'Multiple timestamp columns ("datetime" dtype) found in entity DataFrame. '
+        "Only one timestamp column is allowed. "
+        "Timestamp columns: ts1, ts2"
+    ) == str(excinfo.value)
+
+
+def test_wrong_type_in_feature_list_raises_error(mock_convert_to_bigquery_dataframe):
+    with pytest.raises(ValueError) as excinfo:
+        bdf = mock_bdf(non_ts_cols=["hi"], ts_cols=["ts1"], sql=None)
+        mock_convert_to_bigquery_dataframe.return_value = bdf
+
+        offline_store.fetch_historical_feature_values(
+            entity_df=pd.DataFrame(),
+            features=[("hi", "bye")],
+        )
+
+    assert (
+        "Unsupported feature type <class 'tuple'> found in feature list. "
+        "Feature: ('hi', 'bye')"
+    ) == str(excinfo.value)
+
+
+# def test_feature_str_with_project_regex():
+# def test_feature_str_without_project_regex():
+
+
+@pytest.fixture()
+def mock_query():
+    with mock.patch.object(bpd, "read_gbq_query") as mock_read_query:
+        yield mock_read_query
+
+
+@pytest.fixture()
+def mock_fg():
+    with mock.patch.object(FeatureGroup, "__new__") as mock_fg:
+        yield mock_fg
+
+
+def create_mock_fg(
+    name: str,
+    entity_id_cols: List[str],
+    bq_uri: str,
+):
+    fg = mock.MagicMock()
+    fg.name = name
+    fg._gca_resource.big_query.entity_id_columns = entity_id_cols
+    fg._gca_resource.big_query.big_query_source.input_uri = bq_uri
+    return fg
+
+
+@pytest.fixture()
+def mock_feature():
+    with mock.patch.object(Feature, "__new__") as mock_feature:
+        yield mock_feature
+
+
+def create_mock_feature(
+    name: str,
+    version_column_name: str,
+):
+    feature = mock.MagicMock()
+    feature.name = name
+    feature.version_column_name = version_column_name
+    return feature
+
+
+def help_test_one_feature(
+    feature_name: str,
+    bq_column_name: str,
+    expected_sql: str,
+    mock_convert_to_bigquery_dataframe,
+    mock_query,
+    mock_fg,
+    mock_feature,
+):
+    entity_df = pd.DataFrame(
+        [
+            CUSTOMER_1_OLD_ENTITY_DF_ENTRY,
+            CUSTOMER_2_OLD_ENTITY_DF_ENTRY,
+        ],
+        columns=ENTITY_DF_COLUMNS,
+    )
+
+    mock_convert_to_bigquery_dataframe.return_value = mock_bdf(
+        non_ts_cols=["customer_id", "feature_1"],
+        ts_cols=["timestamp"],
+        sql=FAKE_ENTITY_DF_QUERY,
+    )
+    mock_query.return_value = "SOME SQL QUERY OUTPUT"
+
+    mock_fg.return_value = create_mock_fg(
+        name="fake", entity_id_cols=["customer_id"], bq_uri="bq://my.table"
+    )
+    mock_feature.return_value = create_mock_feature(
+        name=feature_name, version_column_name=bq_column_name
+    )
+    mock_fg.return_value.get_feature = mock_feature
+
+    rsp = offline_store.fetch_historical_feature_values(
+        entity_df=entity_df,
+        features=[f"fake.{feature_name}"],
+    )
+
+    mock_query.assert_called_once_with(
+        expected_sql,
+        index_col=bigframes.enums.DefaultIndexKind.NULL,
+    )
+
+    assert rsp == "SOME SQL QUERY OUTPUT"
+
+
+def test_one_feature_same_bq_col_name(
+    mock_convert_to_bigquery_dataframe, mock_query, mock_fg, mock_feature
+):
+    help_test_one_feature(
+        "my_feature",
+        "my_feature",
+        """WITH
+  entity_df_without_row_num AS (
+    ENTITY DF QUERY Line 1
+    ENTITY DF QUERY Line 2
+    ENTITY DF QUERY Line 3
+  ),
+  entity_df AS (
+    SELECT *, ROW_NUMBER() OVER() AS row_num,
+    FROM entity_df_without_row_num
+  ),
+  # Features
+  fake__my_feature AS (
+    SELECT
+      customer_id,
+      my_feature AS my_feature,
+      feature_timestamp
+    FROM my.table
+  )
+
+SELECT
+  entity_df.customer_id, entity_df.feature_1,
+  fake__my_feature.my_feature,
+  entity_df.timestamp
+
+FROM entity_df
+LEFT OUTER JOIN fake__my_feature ON
+  (
+   entity_df.customer_id = fake__my_feature.customer_id AND
+   CAST(fake__my_feature.feature_timestamp AS TIMESTAMP) <= CAST(entity_df.timestamp AS TIMESTAMP)
+  )
+QUALIFY ROW_NUMBER() OVER (PARTITION BY entity_df.row_num ORDER BY timestamp DESC) = 1""",
+        mock_convert_to_bigquery_dataframe,
+        mock_query,
+        mock_fg,
+        mock_feature,
+    )
+
+
+def test_one_feature_bq_col_name_is_different(
+    mock_convert_to_bigquery_dataframe, mock_query, mock_fg, mock_feature
+):
+    help_test_one_feature(
+        "my_feature",
+        "bq_col_name",
+        """WITH
+  entity_df_without_row_num AS (
+    ENTITY DF QUERY Line 1
+    ENTITY DF QUERY Line 2
+    ENTITY DF QUERY Line 3
+  ),
+  entity_df AS (
+    SELECT *, ROW_NUMBER() OVER() AS row_num,
+    FROM entity_df_without_row_num
+  ),
+  # Features
+  fake__my_feature AS (
+    SELECT
+      customer_id,
+      bq_col_name AS my_feature,
+      feature_timestamp
+    FROM my.table
+  )
+
+SELECT
+  entity_df.customer_id, entity_df.feature_1,
+  fake__my_feature.my_feature,
+  entity_df.timestamp
+
+FROM entity_df
+LEFT OUTER JOIN fake__my_feature ON
+  (
+   entity_df.customer_id = fake__my_feature.customer_id AND
+   CAST(fake__my_feature.feature_timestamp AS TIMESTAMP) <= CAST(entity_df.timestamp AS TIMESTAMP)
+  )
+QUALIFY ROW_NUMBER() OVER (PARTITION BY entity_df.row_num ORDER BY timestamp DESC) = 1""",
+        mock_convert_to_bigquery_dataframe,
+        mock_query,
+        mock_fg,
+        mock_feature,
+    )
--- vertexai/resources/preview/feature_store/offline_store.py
+++ vertexai/resources/preview/feature_store/offline_store.py
@@ -15,12 +15,239 @@
 # limitations under the License.
 #
 
-try:
-    # pylint: disable=g-import-not-at-top
-    import bigframes.pandas as bpd
-except ImportError:
-    bpd = None
+import datetime
+import re
+
+from typing import Optional, List, Tuple, Union, TYPE_CHECKING
+from vertexai.resources.preview.feature_store import (
+    FeatureGroup,
+    Feature,
+)
+
+from . import _offline_store_impl as impl
 
 
-def fetch_historical_feature_values() -> "bpd.DataFrame":
-    raise NotImplementedError()
+if TYPE_CHECKING:
+    try:
+        import bigframes
+    except ImportError:
+        bigframes = None
+
+    try:
+        import pandas as pd
+    except ImportError:
+        pd = None
+
+
+def _try_import_bigframes():
+    """Try to import `bigframes` and return it if successful - otherwise raise an import error."""
+    try:
+        import bigframes
+        import bigframes.pandas
+
+        return bigframes
+    except ImportError as exc:
+        raise ImportError(
+            "`bigframes` is not installed but required for this functionality."
+        ) from exc
+
+
+def _get_feature_group_from_feature(feature: Feature):
+    """Given a feature, return the feature group resource."""
+    result = re.fullmatch(
+        r"projects/.+/locations/.+/featureGroups/(?P<feature_group>.+)/features/.+",
+        feature.resource_name,
+    )
+
+    if not result:
+        raise ValueError("Couldn't find feature group in feature.")
+
+    project = feature.project
+    feature_group = result.group("feature_group")
+
+    return FeatureGroup(feature_group, project=project)
+
+
+def _extract_feature_from_str_repr(str_feature: str) -> Tuple[FeatureGroup, Feature]:
+    """Given a feature in string representation, return the feature and feature group."""
+    # TODO: compile expr + place it in a constant
+    result = re.fullmatch(
+        r"((?P<project>.*)\.)?(?P<feature_group>.*)\.(?P<feature>.*)",
+        str_feature,
+    )
+    if not result:
+        raise ValueError(
+            f"Feature '{str_feature}' is a string but not in expected format 'feature_group.feature' or 'project.feature_group.feature'."
+        )
+
+    if result.group("project"):
+        feature_group = FeatureGroup(
+            result.group("feature_group"), project=result.group("project")
+        )
+        feature = feature_group.get_feature(result.group("feature"))
+    else:
+        feature_group = FeatureGroup(result.group("feature_group"))
+        feature = feature_group.get_feature(result.group("feature"))
+
+    return (feature_group, feature)
+
+
+def _feature_to_data_source(
+    feature_group: FeatureGroup, feature: Feature
+) -> impl.DataSource:
+    qualifying_name = f"{feature_group.name}__{feature.name}"
+    gbq_column = feature.version_column_name
+    assert gbq_column
+
+    column_name = feature.name
+    assert column_name
+
+    timestamp_column = "feature_timestamp"
+
+    # TODO: Expose entity_id_columns as a property in FeatureGroup
+    entity_id_columns = feature_group._gca_resource.big_query.entity_id_columns
+    assert entity_id_columns
+
+    bq_uri = feature_group._gca_resource.big_query.big_query_source.input_uri
+    assert bq_uri
+
+    fully_qualified_table = bq_uri.lstrip("bq://")
+    assert fully_qualified_table
+
+    query = (
+        f"SELECT\n"
+        f'  {", ".join(entity_id_columns)},\n'
+        f"  {gbq_column} AS {column_name},\n"
+        f"  {timestamp_column}\n"
+        f"FROM {fully_qualified_table}"
+    )
+
+    return impl.DataSource(
+        qualifying_name=qualifying_name,
+        sql=query,
+        data_columns=[column_name],
+        # TODO: this will be parameterized in the future
+        timestamp_column=timestamp_column,
+        entity_id_columns=entity_id_columns,
+    )
+
+
+class _DataFrameToBigQueryDataFramesConverter:
+    @classmethod
+    def to_bigquery_dataframe(cls, df: "pd.DataFrame") -> "bigframes.pandas.DataFrame":
+        bigframes = _try_import_bigframes()
+        return bigframes.pandas.DataFrame(data=df)
+
+
+def fetch_historical_feature_values(
+    entity_df: "bigframes.pandas.DataFrame",
+    # TODO: Add support for FeatureView | FeatureGroup | bigframes.pandas.DataFrame
+    features: List[Union[str, Feature]],
+    # TODO: Add support for feature_age_threshold
+    feature_age_threshold: Optional[datetime.timedelta] = None,
+    dry_run: bool = False,
+) -> "Union[bigframes.pandas.DataFrame, None]":
+    """Fetch historical data at the timestamp specified for each entity.
+
+    Feature data will be joined by matching their entity_id_column(s) with
+    corresponding columns in the entity data frame.
+
+    If feature names need to be remapped use the `name_remapper` argument.
+
+    ... some explanation/examples here...
+
+    Args:
+      entity_df:
+        An entity DataFrame where one/multiple columns have entity ID.
+        One column should have a timestamp (used for feature lookup). Other
+        columns may have feature data. Entity IDs may be repeated with
+        different timestamp values (in the timestamp column) to lookup data for
+        entities at different points in time.
+      features:
+        Feature data will be joined with the entity data frame.
+         * If `str` is given use `project.feature_group.feature` as the format.
+          `project_id.feature_group_id.feature_id` may be used if features are
+          in another project.
+         * If `FeatureView` is given, the *sources* of the FeatureView will be
+           used - but data will be read from the backing BigQuery table.
+      feature_age_threshold:
+        How far back from the timestamp to look for features values. If no
+        feature values are found, empty/null value will be populated.
+      dry_run:
+        Build the Point-In-Time Lookup (PITL) query but don't run it. The PITL
+        query will be printed to stdout.
+
+    Returns:
+      A `bigframes.pandas.DataFrame` with the historical feature values. `None`
+      if in `dry_run` mode.
+    """
+
+    bigframes = _try_import_bigframes()
+
+    if feature_age_threshold is not None:
+        raise NotImplementedError("feature_age_threshold is not yet supported.")
+
+    if not features:
+        raise ValueError("Please specify a non-empty list of features.")
+
+    # Convert to bigframe if needed.
+    if not isinstance(entity_df, bigframes.pandas.DataFrame):
+        entity_df = _DataFrameToBigQueryDataFramesConverter.to_bigquery_dataframe(
+            entity_df
+        )
+
+    # Ensure one timestamp column is present in the entity DataFrame.
+    ts_cols = entity_df.select_dtypes(include=["datetime"]).columns
+    if len(ts_cols) > 1:
+        # TODO: Support multiple timestamp columns by specifying feature_timestamp column in an override.
+        raise ValueError(
+            'Multiple timestamp columns ("datetime" dtype) found in entity DataFrame. '
+            "Only one timestamp column is allowed. "
+            f"Timestamp columns: {', '.join([col for col in ts_cols])}"
+        )
+    elif len(ts_cols) == 0:
+        raise ValueError(
+            'No timestamp column ("datetime" dtype) found in entity DataFrame.'
+        )
+    entity_df_ts_col = ts_cols[0]
+    entity_df_non_ts_cols = [c for c in entity_df.columns if c != entity_df_ts_col]
+    entity_data_source = impl.DataSource(
+        qualifying_name="entity_df",
+        sql=entity_df.sql,
+        data_columns=entity_df_non_ts_cols,
+        timestamp_column=entity_df_ts_col,
+    )
+
+    feature_data: List[impl.DataSource] = []
+    for feature in features:
+        if isinstance(feature, Feature):
+            feature_group = _get_feature_group_from_feature(feature)
+            feature_data.append(_feature_to_data_source(feature_group, feature))
+        elif isinstance(feature, str):
+            feature_group, feature = _extract_feature_from_str_repr(feature)
+            feature_data.append(_feature_to_data_source(feature_group, feature))
+        else:
+            raise ValueError(
+                f"Unsupported feature type {type(feature)} found in feature list. Feature: {feature}"
+            )
+
+    # TODO: Verify `feature_data`.
+    #  * Ensure that qualifying_names are not interfering.
+    #  * Ensure that feature names are not interfering.
+    #  * Ensure that entity id columns of all features are present in the entity DF.
+
+    query = impl.render_pitl_query(
+        entity_data=entity_data_source,
+        feature_data=feature_data,
+    )
+
+    if dry_run:
+        print("--- Dry run mode: PITL QUERY BEGIN ---")
+        print(query)
+        print("--- Dry run mode: PITL QUERY END ---")
+        return None
+
+    return bigframes.pandas.read_gbq_query(
+        query,
+        index_col=bigframes.enums.DefaultIndexKind.NULL,
+    )
