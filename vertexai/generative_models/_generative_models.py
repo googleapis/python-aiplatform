9f37edfb200c28b5db802fcec1cb0b772f50ecc3
Alexey Volkov <avolkov@google.com>
2024-08-05 14:26:41 -0700

    feat: GenAI - Added `is_single_text` property to `GenerationResponse`, `Candidate`, `Content`. Added `Part.is_text`
    PiperOrigin-RevId: 659681877

--- vertexai/generative_models/_generative_models.py
+++ vertexai/generative_models/_generative_models.py
@@ -2043,6 +2043,10 @@ class GenerationResponse:
 
     # GenerationPart properties
     @property
+    def is_single_text(self) -> bool:
+        return len(self.candidates) == 1 and self.candidates[0].is_single_text
+
+    @property
     def text(self) -> str:
         if len(self.candidates) > 1:
             raise ValueError(
@@ -2140,6 +2144,10 @@ class Candidate:
 
     # GenerationPart properties
     @property
+    def is_single_text(self) -> bool:
+        return self.content.is_single_text
+
+    @property
     def text(self) -> str:
         try:
             return self.content.text
@@ -2216,6 +2224,10 @@ class Content:
         self._raw_content.role = role
 
     # GenerationPart properties
+    @property
+    def is_single_text(self) -> bool:
+        return len(self.parts) == 1 and self.parts[0].is_text
+
     @property
     def text(self) -> str:
         if len(self.parts) > 1:
@@ -2308,6 +2320,10 @@ class Part:
 
     def to_dict(self) -> Dict[str, Any]:
         return _proto_to_dict(self._raw_part)
+
+    @property
+    def is_text(self) -> bool:
+        return "text" in self._raw_part
 
     @property
     def text(self) -> str:
