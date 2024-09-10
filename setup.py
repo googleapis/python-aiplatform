95dde4b46c30bde369a6b41d389b7c3c07849030
michelle-melton <michelle.melton@mailchimp.com>
2024-09-10 10:10:12 -0400

    "fastapi >= 0.71.0"

--- setup.py
+++ setup.py
@@ -83,10 +83,10 @@ vizier_extra_require = [
 
 prediction_extra_require = [
     "docker >= 5.0.3",
-    "fastapi >= 0.112.3",
+    "fastapi >= 0.71.0",
     "httpx >=0.23.0, <0.25.0",  # Optional dependency of fastapi
-    "starlette >= 0.37.1",
-    "uvicorn[standard] >=0.30.6",
+    "starlette >= 0.17.1",
+    "uvicorn[standard] >= 0.16.0",
 ]
 
 endpoint_extra_require = ["requests >= 2.28.1"]
