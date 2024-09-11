2fe7552d4253ac2e2a1217adb9e32ced563133d7
Amy Wu <wuamy@google.com>
2024-09-10 19:22:29 -0700

    chore: Update fastapi version upper bound to 0.114.0
    PiperOrigin-RevId: 673191968

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ vizier_extra_require = [
 
 prediction_extra_require = [
     "docker >= 5.0.3",
-    "fastapi >= 0.71.0",
+    "fastapi >= 0.71.0, <=0.114.0",
     "httpx >=0.23.0, <0.25.0",  # Optional dependency of fastapi
     "starlette >= 0.17.1",
     "uvicorn[standard] >= 0.16.0",
