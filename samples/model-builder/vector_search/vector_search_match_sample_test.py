ea8ae2dab5f9d8535cd278fb689e6a277a074007
A Vertex SDK engineer <vertex-sdk-bot@google.com>
2024-12-04 09:26:42 -0800

    chore: Samples - Add vector search sample for PSC match queries
    PiperOrigin-RevId: 702752681

--- samples/model-builder/vector_search/vector_search_match_sample_test.py
+++ samples/model-builder/vector_search/vector_search_match_sample_test.py
@@ -75,3 +75,72 @@ def test_vector_search_match_jwt_sample(
         num_neighbors=10,
         signed_jwt=constants.VECTOR_SEARCH_PRIVATE_ENDPOINT_SIGNED_JWT,
     )
+
+
+def test_vector_search_match_psc_manual_sample(
+    mock_sdk_init,
+    mock_index_endpoint,
+    mock_index_endpoint_init,
+    mock_index_endpoint_match
+):
+    vector_search_match_sample.vector_search_match_psc_manual(
+        project=constants.PROJECT,
+        location=constants.LOCATION,
+        index_endpoint_name=constants.VECTOR_SEARCH_INDEX_ENDPOINT,
+        deployed_index_id=constants.VECTOR_SEARCH_DEPLOYED_INDEX_ID,
+        queries=constants.VECTOR_SERACH_INDEX_QUERIES,
+        num_neighbors=10,
+        ip_address=constants.VECTOR_SEARCH_PSC_MANUAL_IP_ADDRESS,
+    )
+
+    # Check client initialization
+    mock_sdk_init.assert_called_with(
+        project=constants.PROJECT, location=constants.LOCATION
+    )
+
+    # Check index endpoint initialization with right index endpoint name
+    mock_index_endpoint_init.assert_called_with(
+        index_endpoint_name=constants.VECTOR_SEARCH_INDEX_ENDPOINT)
+
+    # Check index endpoint PSC IP address is set
+    assert mock_index_endpoint.private_service_connect_ip_address == (
+        constants.VECTOR_SEARCH_PSC_MANUAL_IP_ADDRESS
+    )
+
+    # Check index_endpoint.match is called with right params.
+    mock_index_endpoint_match.assert_called_with(
+        deployed_index_id=constants.VECTOR_SEARCH_DEPLOYED_INDEX_ID,
+        queries=constants.VECTOR_SERACH_INDEX_QUERIES,
+        num_neighbors=10,
+    )
+
+
+def test_vector_search_match_psc_automation_sample(
+    mock_sdk_init, mock_index_endpoint_init, mock_index_endpoint_match
+):
+    vector_search_match_sample.vector_search_match_psc_automation(
+        project=constants.PROJECT,
+        location=constants.LOCATION,
+        index_endpoint_name=constants.VECTOR_SEARCH_INDEX_ENDPOINT,
+        deployed_index_id=constants.VECTOR_SEARCH_DEPLOYED_INDEX_ID,
+        queries=constants.VECTOR_SERACH_INDEX_QUERIES,
+        num_neighbors=10,
+        psc_network=constants.VECTOR_SEARCH_VPC_NETWORK,
+    )
+
+    # Check client initialization
+    mock_sdk_init.assert_called_with(
+        project=constants.PROJECT, location=constants.LOCATION
+    )
+
+    # Check index endpoint initialization with right index endpoint name
+    mock_index_endpoint_init.assert_called_with(
+        index_endpoint_name=constants.VECTOR_SEARCH_INDEX_ENDPOINT)
+
+    # Check index_endpoint.match is called with right params.
+    mock_index_endpoint_match.assert_called_with(
+        deployed_index_id=constants.VECTOR_SEARCH_DEPLOYED_INDEX_ID,
+        queries=constants.VECTOR_SERACH_INDEX_QUERIES,
+        num_neighbors=10,
+        psc_network=constants.VECTOR_SEARCH_VPC_NETWORK,
+    )
