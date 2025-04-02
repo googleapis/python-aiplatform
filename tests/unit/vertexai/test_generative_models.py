6993edf38c5552c4e435d0be283c206ef0d9b18a
A Vertex SDK engineer <vertex-sdk-bot@google.com>
2025-04-02 09:14:05 -0700

    feat: Model optimizer SDK support
    PiperOrigin-RevId: 743163479

--- tests/unit/vertexai/test_generative_models.py
+++ tests/unit/vertexai/test_generative_models.py
@@ -991,6 +991,25 @@ class TestGenerativeModels:
         "generative_models",
         [generative_models, preview_generative_models],
     )
+    def test_generate_content_model_optimizer(
+        self, generative_models: generative_models
+    ):
+        model = generative_models.GenerativeModel("model-optimizer")
+
+        response = model.generate_content("Why is sky blue?",
+            generation_config=generative_models.GenerationConfig(
+                model_config=generative_models.GenerationConfig.ModelConfig(
+                    feature_selection_preference=generative_models.GenerationConfig.ModelConfig.FeatureSelectionPreference.BALANCED
+                )
+            ),
+        )
+        assert response.text
+
+    @patch_genai_services
+    @pytest.mark.parametrize(
+        "generative_models",
+        [generative_models, preview_generative_models],
+    )
     def test_chat_send_message(self, generative_models: generative_models):
         model = generative_models.GenerativeModel("gemini-pro")
         chat = model.start_chat()
