547bf01410647b104c6b8134d57b8c27847558b4
Yeesian Ng <ysian@google.com>
2025-04-24 15:12:05 -0700

    feat: Update upperbound for langgraph
    PiperOrigin-RevId: 751155299

--- setup.py
+++ setup.py
@@ -179,7 +179,7 @@ langchain_extra_require = [
     "langchain >= 0.3, < 0.4",
     "langchain-core >= 0.3, < 0.4",
     "langchain-google-vertexai >= 2, < 3",
-    "langgraph >= 0.2.45, < 0.3",
+    "langgraph >= 0.2.45, < 0.4",
     "openinference-instrumentation-langchain >= 0.1.19, < 0.2",
 ]
 
