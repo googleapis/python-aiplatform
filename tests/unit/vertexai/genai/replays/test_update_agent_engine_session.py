34920e82e20515a3b7337729b8598bc3272405b6
Shawn Yang <xiaolongyang@google.com>
2025-06-27 14:43:06 -0700

    feat: GenAI SDK client - Agent Engine Session SDK
    PiperOrigin-RevId: 776723945

---
+++ tests/unit/vertexai/genai/replays/test_update_agent_engine_session.py
@@ -0,0 +1,44 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# pylint: disable=protected-access,bad-continuation,missing-function-docstring
+
+from tests.unit.vertexai.genai.replays import pytest_helper
+from vertexai._genai import types
+
+
+def test_update_session(client):
+    agent_engine = client.agent_engines.create()
+    operation = client.agent_engines.update_session(
+        name=agent_engine.api_resource.name,
+        user_id="test-user-123",
+    )
+    session = operation.response
+    operation = client.agent_engines.update_session(
+        name=session.name,
+        config=types.UpdateAgentEngineSessionConfig(
+            display_name="my_updated_session",
+        ),
+    )
+    assert isinstance(operation, types.AgentEngineSessionOperation)
+    assert operation.response.display_name == "my_updated_session"
+    assert operation.response.user_id == "test-user-123"
+    assert operation.response.name == session.name
+
+
+pytestmark = pytest_helper.setup(
+    file=__file__,
+    globals_for_file=globals(),
+    test_method="agent_engines.update_session",
+)
