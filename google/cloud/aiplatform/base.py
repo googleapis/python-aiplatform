afb53858469fa7f999990e8be4f53e86d307d9ed
Oleh Prypin <oprypin@google.com>
2025-08-25 10:46:28 -0700

    chore: Remove usage of `@classmethod`+`@property` (invalid from Python 3.13 onwards)
    PiperOrigin-RevId: 799184402

--- google/cloud/aiplatform/base.py
+++ google/cloud/aiplatform/base.py
@@ -1505,14 +1505,10 @@ class PreviewMixin(abc.ABC):
     class allows the child class to introduce preview features.
     """
 
-    @classmethod
-    @property
-    @abc.abstractmethod
-    def _preview_class(cls: Type[PreviewClass]) -> Type[PreviewClass]:
-        """Class that is currently in preview or has a preview feature.
-        Class must have `resource_name` and `credentials` attributes.
-        """
-        pass
+    _preview_class: Type[PreviewClass]
+    """Class that is currently in preview or has a preview feature.
+    Class must have `resource_name` and `credentials` attributes.
+    """
 
     @property
     def preview(self) -> PreviewClass:
