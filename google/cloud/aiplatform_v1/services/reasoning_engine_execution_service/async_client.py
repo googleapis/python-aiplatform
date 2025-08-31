aeb3f90220ebf5e117a583fee6962e26d3d37a17
uncured7036 <oaf8840kado@outlook.com>
2025-08-31 21:50:12 +0800

    fix: make async_stream_query fully async

--- google/cloud/aiplatform_v1/services/reasoning_engine_execution_service/async_client.py
+++ google/cloud/aiplatform_v1/services/reasoning_engine_execution_service/async_client.py
@@ -410,7 +410,7 @@ class ReasoningEngineExecutionServiceAsyncClient:
         # Done; return the response.
         return response
 
-    def stream_query_reasoning_engine(
+    async def stream_query_reasoning_engine(
         self,
         request: Optional[
             Union[
@@ -543,7 +543,7 @@ class ReasoningEngineExecutionServiceAsyncClient:
         self._client._validate_universe_domain()
 
         # Send the request.
-        response = rpc(
+        response = await rpc(
             request,
             retry=retry,
             timeout=timeout,
