7dd2629d826377560ad9ac3c0e24724e80c7e1bc
A Vertex SDK engineer <vertex-sdk-bot@google.com>
2025-10-17 18:26:27 -0700

    feat: Add STZ support for Endpoint.deploy and Model.deploy (preview).
    PiperOrigin-RevId: 820898193

--- tests/unit/aiplatform/test_models.py
+++ tests/unit/aiplatform/test_models.py
@@ -2829,6 +2829,68 @@ class TestModel:
         )
 
     @pytest.mark.usefixtures(
+        "get_endpoint_mock",
+        "get_model_mock",
+        "create_endpoint_mock",
+        "preview_deploy_model_mock",
+    )
+    @pytest.mark.parametrize("sync", [True, False])
+    def test_preview_deploy_no_endpoint_with_stz_config(
+        self, preview_deploy_model_mock, sync
+    ):
+        test_model = preview_models.Model(_TEST_ID)
+        test_model._gca_resource.supported_deployment_resources_types.append(
+            aiplatform.gapic.Model.DeploymentResourcesType.DEDICATED_RESOURCES
+        )
+
+        test_endpoint = test_model.deploy(
+            machine_type=_TEST_MACHINE_TYPE,
+            accelerator_type=_TEST_ACCELERATOR_TYPE,
+            accelerator_count=_TEST_ACCELERATOR_COUNT,
+            sync=sync,
+            deploy_request_timeout=None,
+            min_replica_count=0,
+            max_replica_count=1,
+            initial_replica_count=1,
+            min_scaleup_period=300,
+            idle_scaledown_period=300,
+        )
+
+        if not sync:
+            test_endpoint.wait()
+
+        expected_machine_spec = gca_machine_resources_v1beta1.MachineSpec(
+            machine_type=_TEST_MACHINE_TYPE,
+            accelerator_type=_TEST_ACCELERATOR_TYPE,
+            accelerator_count=_TEST_ACCELERATOR_COUNT,
+        )
+        expected_dedicated_resources = gca_machine_resources_v1beta1.DedicatedResources(
+            machine_spec=expected_machine_spec,
+            min_replica_count=0,
+            max_replica_count=1,
+            initial_replica_count=1,
+            scale_to_zero_spec=gca_machine_resources_v1beta1.DedicatedResources.ScaleToZeroSpec(
+                min_scaleup_period=duration_pb2.Duration(seconds=300),
+                idle_scaledown_period=duration_pb2.Duration(seconds=300),
+            ),
+        )
+        expected_deployed_model = gca_endpoint_v1beta1.DeployedModel(
+            dedicated_resources=expected_dedicated_resources,
+            model=test_model.resource_name,
+            display_name=None,
+            faster_deployment_config=gca_endpoint_v1beta1.FasterDeploymentConfig(),
+            enable_container_logging=True,
+        )
+
+        preview_deploy_model_mock.assert_called_once_with(
+            endpoint=test_endpoint.resource_name,
+            deployed_model=expected_deployed_model,
+            traffic_split={"0": 100},
+            metadata=(),
+            timeout=None,
+        )
+
+    @pytest.mark.usefixtures(
         "get_endpoint_mock", "get_model_mock", "create_endpoint_mock"
     )
     @pytest.mark.parametrize("sync", [True, False])
