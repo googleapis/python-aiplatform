f3c96e27d3fc9fc7809cdc24ec476ad37edb717a
Jaycee Li <jayceeli@google.com>
2025-10-22 13:07:43 -0700

    chore: Clean up internal configurations
    PiperOrigin-RevId: 822709885

--- vertexai/_genai/types.py
+++ vertexai/_genai/types.py
@@ -9701,7 +9701,7 @@ class SchemaTablesDatasetMetadata(_common.BaseModel):
     """Represents the metadata schema for multimodal dataset metadata."""
 
     input_config: Optional[SchemaTablesDatasetMetadataInputConfig] = Field(
-        default=None, description=""""""
+        default=None, description="""inputConfig"""
     )
 
 
@@ -9709,7 +9709,7 @@ class SchemaTablesDatasetMetadataDict(TypedDict, total=False):
     """Represents the metadata schema for multimodal dataset metadata."""
 
     input_config: Optional[SchemaTablesDatasetMetadataInputConfigDict]
-    """"""
+    """inputConfig"""
 
 
 SchemaTablesDatasetMetadataOrDict = Union[
