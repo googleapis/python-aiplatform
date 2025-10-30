a812943e658a71f0ec25fc27aa9195a8185db236
A Vertex SDK engineer <vertex-sdk-bot@google.com>
2025-10-29 18:20:38 -0700

    chore!: Switch `cloudtrace.googleapis.com` to `telemetry.googleapis.com` for tracing API.
    PiperOrigin-RevId: 825791207

--- tests/unit/vertex_adk/test_agent_engine_templates_adk.py
+++ tests/unit/vertex_adk/test_agent_engine_templates_adk.py
@@ -18,7 +18,6 @@ import json
 import os
 from unittest import mock
 from typing import Optional
-import dataclasses
 
 from google import auth
 import vertexai
@@ -97,27 +96,11 @@ def vertexai_init_mock():
 
 
 @pytest.fixture
-def cloud_trace_exporter_mock():
-    import sys
-    import opentelemetry
-
-    mock_cloud_trace_exporter = mock.Mock()
-
-    opentelemetry.exporter = type(sys)("exporter")
-    opentelemetry.exporter.cloud_trace = type(sys)("cloud_trace")
-    opentelemetry.exporter.cloud_trace.CloudTraceSpanExporter = (
-        mock_cloud_trace_exporter
-    )
-
-    sys.modules["opentelemetry.exporter"] = opentelemetry.exporter
-    sys.modules["opentelemetry.exporter.cloud_trace"] = (
-        opentelemetry.exporter.cloud_trace
-    )
-
-    yield mock_cloud_trace_exporter
-
-    del sys.modules["opentelemetry.exporter.cloud_trace"]
-    del sys.modules["opentelemetry.exporter"]
+def otlp_span_exporter_mock():
+    with mock.patch(
+        "opentelemetry.exporter.otlp.proto.http.trace_exporter.OTLPSpanExporter"
+    ) as otlp_span_exporter_mock:
+        yield otlp_span_exporter_mock
 
 
 @pytest.fixture
@@ -609,9 +592,9 @@ class TestAdkApp:
     )
     def test_tracing_setup(
         self,
-        trace_provider_mock: mock.Mock,
-        cloud_trace_exporter_mock: mock.Mock,
         monkeypatch,
+        trace_provider_mock: mock.Mock,
+        otlp_span_exporter_mock: mock.Mock,
     ):
         monkeypatch.setattr(
             "uuid.uuid4", lambda: uuid.UUID("12345678123456781234567812345678")
@@ -633,17 +616,9 @@ class TestAdkApp:
             "some-attribute": "some-value",
         }
 
-        @dataclasses.dataclass
-        class RegexMatchingAll:
-            keys: set[str]
-
-            def __eq__(self, regex: object) -> bool:
-                return isinstance(regex, str) and set(regex.split("|")) == self.keys
-
-        cloud_trace_exporter_mock.assert_called_once_with(
-            project_id=_TEST_PROJECT,
-            client=mock.ANY,
-            resource_regex=RegexMatchingAll(keys=set(expected_attributes.keys())),
+        otlp_span_exporter_mock.assert_called_once_with(
+            session=mock.ANY,
+            endpoint="https://telemetry.googleapis.com/v1/traces",
         )
 
         assert (
@@ -655,7 +630,6 @@ class TestAdkApp:
     def test_enable_tracing(
         self,
         caplog,
-        cloud_trace_exporter_mock,
         tracer_provider_mock,
         simple_span_processor_mock,
     ):
