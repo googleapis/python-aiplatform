5cbbf932fc0cb3c2f442ad988c21042de1f4897f
Sara Robinson <sararob@google.com>
2025-11-19 13:24:10 -0800

    chore: Update docs nox sessions to use virtualenv as backend
    PiperOrigin-RevId: 834420031

--- noxfile.py
+++ noxfile.py
@@ -459,7 +459,7 @@ def cover(session):
     session.run("coverage", "erase")
 
 
-@nox.session(python="3.9")
+@nox.session(python="3.9", venv_backend="virtualenv")
 def docs(session):
     """Build the docs for this library."""
 
@@ -483,7 +483,7 @@ def docs(session):
     )
 
 
-@nox.session(python="3.10")
+@nox.session(python="3.10", venv_backend="virtualenv")
 def docfx(session):
     """Build the docfx yaml files for this library."""
 
@@ -519,7 +519,7 @@ def docfx(session):
     )
 
 
-@nox.session(python="3.9")
+@nox.session(python="3.9", venv_backend="virtualenv")
 def gemini_docs(session):
     """Build the docs for library related to Gemini."""
 
@@ -540,7 +540,7 @@ def gemini_docs(session):
     )
 
 
-@nox.session(python="3.10")
+@nox.session(python="3.10", venv_backend="virtualenv")
 def gemini_docfx(session):
     """Build the docfx yaml files for library related to Gemini."""
 
