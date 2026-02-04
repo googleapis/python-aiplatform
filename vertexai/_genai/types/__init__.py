a5748fd4f09239e437cdf11efb24d22746b35174
Jeff Scudder <jscudder@google.com>
2026-02-03 19:58:53 -0800

    feat: Update data types from discovery doc.
    PiperOrigin-RevId: 865167068

--- vertexai/_genai/types/__init__.py
+++ vertexai/_genai/types/__init__.py
@@ -395,6 +395,7 @@ from .common import ExecuteCodeAgentEngineSandboxConfigOrDict
 from .common import ExecuteSandboxEnvironmentResponse
 from .common import ExecuteSandboxEnvironmentResponseDict
 from .common import ExecuteSandboxEnvironmentResponseOrDict
+from .common import Framework
 from .common import GcsSource
 from .common import GcsSourceDict
 from .common import GcsSourceOrDict
@@ -880,6 +881,12 @@ from .common import SchemaPromptInstancePromptExecutionOrDict
 from .common import SchemaPromptInstanceVariableValue
 from .common import SchemaPromptInstanceVariableValueDict
 from .common import SchemaPromptInstanceVariableValueOrDict
+from .common import SchemaPromptSpecAppBuilderData
+from .common import SchemaPromptSpecAppBuilderDataDict
+from .common import SchemaPromptSpecAppBuilderDataLinkedResource
+from .common import SchemaPromptSpecAppBuilderDataLinkedResourceDict
+from .common import SchemaPromptSpecAppBuilderDataLinkedResourceOrDict
+from .common import SchemaPromptSpecAppBuilderDataOrDict
 from .common import SchemaPromptSpecMultimodalPrompt
 from .common import SchemaPromptSpecMultimodalPromptDict
 from .common import SchemaPromptSpecMultimodalPromptOrDict
@@ -1779,6 +1786,12 @@ __all__ = [
     "SchemaPromptSpecMultimodalPrompt",
     "SchemaPromptSpecMultimodalPromptDict",
     "SchemaPromptSpecMultimodalPromptOrDict",
+    "SchemaPromptSpecAppBuilderDataLinkedResource",
+    "SchemaPromptSpecAppBuilderDataLinkedResourceDict",
+    "SchemaPromptSpecAppBuilderDataLinkedResourceOrDict",
+    "SchemaPromptSpecAppBuilderData",
+    "SchemaPromptSpecAppBuilderDataDict",
+    "SchemaPromptSpecAppBuilderDataOrDict",
     "SchemaPromptSpecPartList",
     "SchemaPromptSpecPartListDict",
     "SchemaPromptSpecPartListOrDict",
@@ -1950,6 +1963,7 @@ __all__ = [
     "Language",
     "MachineConfig",
     "State",
+    "Framework",
     "EvaluationItemType",
     "SamplingMethod",
     "RubricContentType",
